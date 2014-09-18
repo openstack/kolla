6564ab4acb5c8c3530f9a727dea13e146f90bd47
Lars Kellogg-Stedman <lars@redhat.com>
2014-09-18 15:30:23 -0400

    added rabbitmq

---
+++ docker/rabbitmq/start.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+: ${RABBITMQ_USER:=guest}
+: ${RABBITMQ_PASS:=guest}
+: ${RABBITMQ_PORT:=5672}
+: ${RABBITMQ_LOG_BASE:=/var/log/rabbitmq}
+
+sed -i '
+	s|@RABBITMQ_USER@|'"$RABBITMQ_USER"'|g
+	s|@RABBITMQ_PASS@|'"$RABBITMQ_PASS"'|g
+' /etc/rabbitmq/rabbitmq.config
+
+sed -i '
+	s|@RABBITMQ_PORT@|'"$RABBITMQ_PORT"'|g
+	s|@RABBITMQ_LOG_BASE@|'"$RABBITMQ_LOG_BASE"'|g
+' /etc/rabbitmq/rabbitmq-env.conf
+
+exec /usr/lib/rabbitmq/bin/rabbitmq-server
+
