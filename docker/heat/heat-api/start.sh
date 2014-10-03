1931a016b06dd1c1b18dc0328497d19d16f024ac
Lars Kellogg-Stedman <lars@redhat.com>
2014-10-03 10:26:18 -0400

    Using new fedora base image from kollaglue
    Change-Id: I388113e5a9c748054a099ef1a3063863120e9a99

--- docker/heat/heat-api/start.sh
+++ docker/heat/heat-api/start.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
-sh /opt/heat/config-glance.sh api
+: ${IP_ADDRESS:=$IP_ADDRESS}
+
+if [ -z "$IP_ADDRESS" ]; then
+	echo >&2 'error: IP_ADDRESS is not set.'
+	echo >&2 '   Be sure to set IP_ADDRESS so it can be placed in heat configurations'
+	exit 1
+fi
+
 
 exec /usr/bin/heat-api
