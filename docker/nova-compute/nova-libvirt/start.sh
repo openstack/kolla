727d8d139ce7f7f3fd87547f755b9f48c56a405e
Steven Dake <sdake@redhat.com>
2014-10-07 11:20:26 -0700

    Get to a functional nova-libvirt start
    Get a working Dockerfile
    Sort out start.sh to run libvirt in a container
    
    Change-Id: Iaafe807b1631ad59fea200a02e46a8cabd6791ca

--- docker/nova-compute/nova-libvirt/start.sh
+++ docker/nova-compute/nova-libvirt/start.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-# placeholder for libvirt startup script
+/bin/chmod 666 /dev/kvm
+
+echo "Starting guests."
+/usr/libexec/libvirt-guests.sh start
+
+echo "Starting virtlockd."
+/usr/sbin/virtlockd &
+
+sleep 3
+
+echo "Starting libvirtd."
+exec /usr/sbin/libvirtd --listen
