a4069300aa285c747846c2aa47d514a282297a65
Lars Kellogg-Stedman <lars@redhat.com>
2014-10-08 12:48:31 -0400

    correct various issues with glance images and configs
    - update keystone endpoints/user on boot (to avoid problems caused by,
      e.g., a stale password or invalid endpoint urls)
    - require GLANCE_DB_PASSWORD and GLANCE_KEYSTONE_PASSWORD in the
      environment, since we start multiple containers
    
    Change-Id: I31214b81280ed34409f92e79003c1116d5737d2e

--- docker/glance/glance-registry/start.sh
+++ docker/glance/glance-registry/start.sh
@@ -2,6 +2,12 @@
 
 . /opt/glance/config-glance.sh
 
+# wait for mysql to start
+while !  mysql -h ${MARIADB_PORT_3306_TCP_ADDR} -u root -p${DB_ROOT_PASSWORD} -e "select 1" mysql > /dev/null 2>&1; do
+    echo "waiting for mysql..."
+    sleep 1
+done
+
 mysql -h ${MARIADB_PORT_3306_TCP_ADDR} -u root -p${DB_ROOT_PASSWORD} mysql <<EOF
 CREATE DATABASE IF NOT EXISTS ${GLANCE_DB_NAME} DEFAULT CHARACTER SET utf8;
 GRANT ALL PRIVILEGES ON ${GLANCE_DB_NAME}.* TO
