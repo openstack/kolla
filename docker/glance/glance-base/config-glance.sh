250bb5f453c41eab1d1b83d5681577893eaff7c8
Daneyon Hansen <danehans@cisco.com>
2014-10-09 17:36:17 +0000

    Closes-Bug: 1379442
    Previously, the glance-api start script would use the container
    IP instead of the proxy IP for the public url when creating the
    keystone endpoint. This causes keystone to provide an unreachable
    IP within the public url and other OS services are unable to
    connect to the public url.
    
    Change-Id: I44befc84a90f59d535a951ee81aaff0fa1ba952d

--- docker/glance/glance-base/config-glance.sh
+++ docker/glance/glance-base/config-glance.sh
@@ -12,7 +12,7 @@ MY_IP=$(ip route get $(ip route | awk '$1 == "default" {print $3}') |
 : ${GLANCE_DB_USER:=glance}
 : ${GLANCE_KEYSTONE_USER:=glance}
 : ${KEYSTONE_AUTH_PROTOCOL:=http}
-: ${PUBLIC_IP:=$MY_IP}
+: ${PUBLIC_IP:=$GLANCE_API_PORT_9292_TCP_ADDR}
 
 if ! [ "$GLANCE_DB_PASSWORD" ]; then
     GLANCE_DB_PASSWORD=$(openssl rand -hex 15)
