a14b8048c273f12aa4b941c0ff923997ad7524d6
Steven Dake <sdake@redhat.com>
2014-10-16 22:59:45 -0700

    Make nova-compute work properly with libvirt
    The nova-compute POD now starts and produces nearly correct output.
    It may be that the libvirt container needs additional attention as it
    is spitting out errors related to the firewall toolchain on the container.
    
    Change-Id: I5bcd21c59f341494206a09e882631185ce66f336

--- docker/nova-compute/nova-compute/start.sh
+++ docker/nova-compute/nova-compute/start.sh
@@ -1,45 +1,7 @@
 #!/bin/sh
 
-. /opt/kolla/kolla-common.sh
+set -e
 
-: ${NOVA_KEYSTONE_USER:=nova}
-: ${ADMIN_TENANT_NAME:=admin}
-
-check_required_vars KEYSTONE_ADMIN_TOKEN KEYSTONE_SERVICE_HOST \
-        NOVA_LIBVIRT_SERVICE
-
-cfg=/etc/nova/nova.conf
-
-crudini --set $cfg DEFAULT admin_token "${KEYSTONE_ADMIN_TOKEN}"
-crudini --sel $cfg DEFAULT log_file ""
-crudini --del $cfg  DEFAULT log_dir
-crudini --set $cfg DEFAULT use_stderr True
-crudini --set $cfg \
-    libvirt \
-    connection_uri \
-    "qemu+tcp://${NOVA_LIBVIRT_SERVICE}/system"
-
-for option in auth_protocol auth_host auth_port; do
-    crudini --del $cfg \
-        keystone_authtoken \
-        $option
-done
-
-crudini --set $cfg \
-    keystone_authtoken \
-    auth_uri \
-    "http://${KEYSTONE_SERVICE_HOST}:5000/"
-crudini --set $cfg \
-    keystone_authtoken \
-    admin_tenant_name \
-    "${ADMIN_TENANT_NAME}"
-crudini --set $cfg \
-    keystone_authtoken \
-    admin_user \
-    "${NOVA_KEYSTONE_USER}"
-crudini --set $cfg \
-    keystone_authtoken \
-    admin_password \
-    "${NOVA_KEYSTONE_PASSWORD}"
+. /opt/kolla/config-nova-compute.sh
 
 exec /usr/bin/nova-compute
