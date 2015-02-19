8a2a8c8a8a44ae2f393a7278d21ff47a5cec9add
Daneyon Hansen <danehans@cisco.com>
2015-02-19 01:27:29 +0000

    Implements: blueprint update-rabbit-container
    Previously, the rabbit container was configured for use in
    a k8s cluster. This patch updates the container to run with fig
    instead of k8s.
    
    Change-Id: Ie04f2f16771c0f733b7b09b7747325bf0d7fb1a9

--- docker/rabbitmq/start.sh
+++ docker/rabbitmq/start.sh
@@ -4,7 +4,7 @@ set -e
 
 : ${RABBITMQ_USER:=guest}
 : ${RABBITMQ_PASS:=guest}
-: ${RABBITMQ_NODE_PORT:=5672}
+: ${RABBITMQ_NODENAME:=rabbit}
 : ${RABBITMQ_LOG_BASE:=/var/log/rabbitmq}
 
 sed -i '
@@ -13,9 +13,8 @@ sed -i '
 ' /etc/rabbitmq/rabbitmq.config
 
 sed -i '
-	s|@RABBITMQ_PORT@|'"$RABBITMQ_NODE_PORT"'|g
+	s|@RABBITMQ_NODENAME@|'"$RABBITMQ_NODENAME"'|g
 	s|@RABBITMQ_LOG_BASE@|'"$RABBITMQ_LOG_BASE"'|g
 ' /etc/rabbitmq/rabbitmq-env.conf
 
-exec /usr/lib/rabbitmq/bin/rabbitmq-server
-
+exec /usr/sbin/rabbitmq-server
