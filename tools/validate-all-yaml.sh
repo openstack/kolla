bfe0d0e0e9d060dd356589c57df9f3e2d07a284f
Martin André <martin.andre@kvhasia.com>
2015-04-14 13:26:31 +0900

    Fix validate-all-yaml.sh to check for files with yml extension
    YAML files can have .yaml or .yml extension. Make `validate-all-yaml.sh`
    check for *.yml files in addition to *.yaml files since we now have some
    of these in our source tree.
    
    This should help to prevent bogus YAML files to land in the repository.
    
    Change-Id: Id59273680fc2424f7b636033b1d641efcaaeba22

--- tools/validate-all-yaml.sh
+++ tools/validate-all-yaml.sh
@@ -3,6 +3,6 @@
 REAL_PATH=$(python -c "import os,sys;print os.path.realpath('$0')")
 cd "$(dirname "$REAL_PATH")/.."
 
-find . -name '*.yaml' -print0 |
+find . -name '*.yaml' -o -name '*.yml' -print0 |
     xargs -0 python tools/validate-yaml.py || exit 1
 
