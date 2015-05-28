7cdd36d3457bc99ab914f864fb12199ab896873d
Steven Dake <stdake@cisco.com>
2015-05-28 10:06:35 -0700

    Make config-linuxbridge-agent.sh bashate compliant
    Change-Id: Ie95c44b92e3d0af56916f0e335afc8fd461b2790
    Partially-Implements: blueprint bashate-gate

--- docker/centos/binary/neutron/neutron-agents/config-scripts/config-linuxbridge-agent.sh
+++ docker/centos/binary/neutron/neutron-agents/config-scripts/config-linuxbridge-agent.sh
@@ -15,15 +15,15 @@ cfg=/etc/neutron/plugins/ml2/ml2_conf.ini
 
 # Configure ml2_conf.ini
 if [[ ${TYPE_DRIVERS} =~ vxlan ]]; then
-  crudini --set $cfg \
-          vxlan \
-          local_ip \
-          "${PUBLIC_IP}"
+    crudini --set $cfg \
+        vxlan \
+        local_ip \
+        "${PUBLIC_IP}"
 fi
 
 crudini --set $cfg \
-        linux_bridge \
-        physical_interface_mappings \
-        "${NEUTRON_FLAT_NETWORK_NAME}:${NEUTRON_FLAT_NETWORK_INTERFACE}"
+    linux_bridge \
+    physical_interface_mappings \
+    "${NEUTRON_FLAT_NETWORK_NAME}:${NEUTRON_FLAT_NETWORK_INTERFACE}"
 
 exec /usr/bin/neutron-linuxbridge-agent --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugins/ml2/ml2_conf.ini --config-dir /etc/neutron
