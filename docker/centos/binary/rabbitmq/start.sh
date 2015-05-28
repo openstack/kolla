58bf3f74ac84ac85fa3e43fe1358cfef5fb78589
Steven Dake <stdake@cisco.com>
2015-05-28 09:28:04 -0700

    Make rabbitmq start.sh bashate compliant
    Change-Id: Iafd0ead3a10354f12e6a1a15855d3d1402bbc523
    Partially-Implements: blueprint bashate-gate

--- docker/centos/binary/rabbitmq/start.sh
+++ docker/centos/binary/rabbitmq/start.sh
@@ -8,13 +8,13 @@ set -e
 : ${RABBITMQ_LOG_BASE:=/var/log/rabbitmq}
 
 sed -i '
-	s|@RABBITMQ_USER@|'"$RABBITMQ_USER"'|g
-	s|@RABBITMQ_PASS@|'"$RABBITMQ_PASS"'|g
+    s|@RABBITMQ_USER@|'"$RABBITMQ_USER"'|g
+    s|@RABBITMQ_PASS@|'"$RABBITMQ_PASS"'|g
 ' /etc/rabbitmq/rabbitmq.config
 
 sed -i '
-	s|@RABBITMQ_NODENAME@|'"$RABBITMQ_NODENAME"'|g
-	s|@RABBITMQ_LOG_BASE@|'"$RABBITMQ_LOG_BASE"'|g
+    s|@RABBITMQ_NODENAME@|'"$RABBITMQ_NODENAME"'|g
+    s|@RABBITMQ_LOG_BASE@|'"$RABBITMQ_LOG_BASE"'|g
 ' /etc/rabbitmq/rabbitmq-env.conf
 
 # work around:
