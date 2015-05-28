1d299dfe9abb26852da238597a903a1a4e44f290
Steven Dake <stdake@cisco.com>
2015-05-28 10:10:00 -0700

    Make config-sudoers.sh bashate compliant
    Change-Id: Ie3fdcf5d3e1cda3b85a6532e462ad78e98e1a378
    Partially-Implements: blueprint bashate-gate

--- docker/centos/binary/neutron/neutron-agents/config-scripts/config-sudoers.sh
+++ docker/centos/binary/neutron/neutron-agents/config-scripts/config-sudoers.sh
@@ -4,9 +4,9 @@
 # Since the container is running in daemon mode, a tty
 # is not present and requiretty must be commented out.
 if [ ! -f /var/run/sudo-modified ]; then
-  chmod 0640 /etc/sudoers
-  sed -i '/Defaults    requiretty/s/^/#/' /etc/sudoers
-  chmod 0440 /etc/sudoers
+    chmod 0640 /etc/sudoers
+    sed -i '/Defaults    requiretty/s/^/#/' /etc/sudoers
+    chmod 0440 /etc/sudoers
 fi
 
 touch /var/run/sudo-modified
