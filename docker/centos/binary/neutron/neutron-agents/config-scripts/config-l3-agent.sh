fe73d3b414cde2bfb52da877069a4f258e3c4416
Steven Dake <stdake@cisco.com>
2015-05-28 10:00:43 -0700

    Make config-l3-agent.sh bashate compliant
    Change-Id: I0ecd173e49fcb9bca9702d506e1af785d3a4983a
    Partially-Implements: blueprint bashate-gate

--- docker/centos/binary/neutron/neutron-agents/config-scripts/config-l3-agent.sh
+++ docker/centos/binary/neutron/neutron-agents/config-scripts/config-l3-agent.sh
@@ -14,71 +14,71 @@ neutron_conf=/etc/neutron/neutron.conf
 
 # Logging
 crudini --set $neutron_conf \
-        DEFAULT \
-        log_file \
-        "${NEUTRON_L3_AGENT_LOG_FILE}"
+    DEFAULT \
+    log_file \
+    "${NEUTRON_L3_AGENT_LOG_FILE}"
 
 # Configure l3_agent.ini
 crudini --set $cfg \
-        DEFAULT \
-        verbose \
-        "${VERBOSE_LOGGING}"
+    DEFAULT \
+    verbose \
+    "${VERBOSE_LOGGING}"
 crudini --set $cfg \
-        DEFAULT \
-        debug \
-        "${DEBUG_LOGGING}"
+    DEFAULT \
+    debug \
+    "${DEBUG_LOGGING}"
 if [[ "${MECHANISM_DRIVERS}" =~ linuxbridge ]] ; then
-  crudini --set $cfg \
-          DEFAULT \
-          interface_driver \
-          "neutron.agent.linux.interface.BridgeInterfaceDriver"
-  crudini --set $cfg \
-          DEFAULT \
-          gateway_external_network_id \
-          ""
-  crudini --set $cfg \
-          DEFAULT \
-          external_network_bridge \
-          ""
+    crudini --set $cfg \
+        DEFAULT \
+        interface_driver \
+        "neutron.agent.linux.interface.BridgeInterfaceDriver"
+    crudini --set $cfg \
+        DEFAULT \
+        gateway_external_network_id \
+        ""
+    crudini --set $cfg \
+        DEFAULT \
+        external_network_bridge \
+        ""
 elif [[ "${MECHANISM_DRIVERS}" =~ .*openvswitch* ]] ; then
-  crudini --set $cfg \
-          DEFAULT \
-          interface_driver \
-          "neutron.agent.linux.interface.OVSInterfaceDriver"
-  crudini --set $cfg \
-          DEFAULT \
-          gateway_external_network_id \
-          "${NEUTRON_FLAT_NETWORK_BRIDGE}"
-  crudini --set $cfg \
-          DEFAULT \
-          external_network_bridge \
-          "${NEUTRON_FLAT_NETWORK_BRIDGE}"
+    crudini --set $cfg \
+        DEFAULT \
+        interface_driver \
+        "neutron.agent.linux.interface.OVSInterfaceDriver"
+    crudini --set $cfg \
+        DEFAULT \
+        gateway_external_network_id \
+        "${NEUTRON_FLAT_NETWORK_BRIDGE}"
+    crudini --set $cfg \
+        DEFAULT \
+        external_network_bridge \
+        "${NEUTRON_FLAT_NETWORK_BRIDGE}"
 fi
 
 crudini --set $cfg \
-        DEFAULT \
-        use_namespaces \
-        "${USE_NAMESPACES}"
+    DEFAULT \
+    use_namespaces \
+    "${USE_NAMESPACES}"
 
 if [ "${USE_NAMESPACES}" == "false" ] ; then
-  source /openrc
-  # Create router if it does not exist
-  /usr/bin/neutron router-list | grep admin-router || /usr/bin/neutron router-create admin-router
-  # Set router-id
-  crudini --set $cfg \
-          DEFAULT \
-          router_id \
-          "$(/usr/bin/neutron router-list | awk '/ admin-router / {print $2}')"
+    source /openrc
+    # Create router if it does not exist
+    /usr/bin/neutron router-list | grep admin-router || /usr/bin/neutron router-create admin-router
+    # Set router-id
+    crudini --set $cfg \
+        DEFAULT \
+        router_id \
+        "$(/usr/bin/neutron router-list | awk '/ admin-router / {print $2}')"
 elif [ "${USE_NAMESPACES}" == "true" ] ; then
-  crudini --set $cfg \
-          DEFAULT \
-          router_delete_namespaces \
-          "true"
+    crudini --set $cfg \
+        DEFAULT \
+        router_delete_namespaces \
+        "true"
 fi
 
 # Remove any existing qrouter namespaces
 ip netns list | grep qrouter | while read -r line ; do
-  ip netns delete $line
+    ip netns delete $line
 done
 
 # Start L3 Agent
