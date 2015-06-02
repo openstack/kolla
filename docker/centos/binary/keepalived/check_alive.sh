52f49094356ca70495e3bc606751d55104726881
Michal Jastrzebski (inc0) <michal.jastrzebski@intel.com>
2015-06-02 04:54:15 +0200

    Keepalived container
    This patch contains keepalived container for yum-based distros.
    
    Change-Id: I8e3ea7a61ad6c27b25b635e7aeeff5ef04a807b4
    Implements: blueprint keepalived-container

---
+++ docker/centos/binary/keepalived/check_alive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This is noop check script. Change it to something meaningful.
+# TODO (inc0): When HAProxy is implemented, this should be changed to HAProxy check
+# https://github.com/stackforge/kolla/blob/master/specs/high-availability.rst
+
+exit 0
