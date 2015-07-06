e21c68a69e476debaf3dc88f022cf4c1bce9ce9e
Swapnil Kulkarni <me@coolsvap.net>
2015-07-06 05:24:47 +0000

    Add config-internal/config-external for horizon
    Partially Implements: Blueprint standard-start
    
    Change-Id: I8a88572a4390824787c663014e6eae52dbb04648

---
+++ docker/common/horizon/config-external.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SOURCE="/etc/openstack-dashboard/local_settings"
+TARGET="/etc/openstack-dashboard/local_settings"
+
+OWNER="horizon"
+
+if [[ -f "$SOURCE" ]]; then
+    cp $SOURCE $TARGET
+    chown ${OWNER}: $TARGET
+    chmod 0644 $TARGET
+fi
