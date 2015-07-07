4c4181d54a5fe5491db8e3f4f29a615005d23c64
Sam Yaple <sam@yaple.net>
2015-07-07 13:51:41 +0000

    Fix permissions on log directory for keystone
    Additionally fixed bootstrap issue caused by an ansible bug in some
    versions of the docker module.
    
    Closes-Bug: #1472270
    Change-Id: Ia5a370db5ab7690854afcd63492ba7dbbea29835

--- docker/common/keystone/start.sh
+++ docker/common/keystone/start.sh
@@ -20,17 +20,18 @@ if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     $CMD
     sleep 5
 
-    openstack service create --name keystone \
-                                --description "OpenStack Identity" identity
-    openstack endpoint create --region "${REGION_NAME}" \
+    keystone service-create --name keystone --type identity \
+                                --description "OpenStack Identity"
+    keystone endpoint-create --region "${REGION_NAME}" \
                                 --publicurl "${PUBLIC_URL}" \
                                 --internalurl "${INTERNAL_URL}" \
-                                --adminurl "${ADMIN_URL}" identity
+                                --adminurl "${ADMIN_URL}" \
+                                --service-id $(keystone service-list | awk '/ identity / {print $2}')
 
-    openstack project create --description "Admin Project" admin
-    openstack user create --password "${KEYSTONE_ADMIN_PASSWORD}" admin
-    openstack role create admin
-    openstack role add --project admin --user admin admin
+    keystone tenant-create --description "Admin Project" --name admin
+    keystone user-create --pass "${KEYSTONE_ADMIN_PASSWORD}" --name admin
+    keystone role-create --name admin
+    keystone user-role-add --user admin --tenant admin --role admin
 
     exit 0
 fi
