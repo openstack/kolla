2207f45d9d50612ced8e75e8a3e8979597f30d85
Jeff Peeler <jpeeler@redhat.com>
2015-07-13 13:40:22 -0400

    Disable ipv6 in gate
    Saw the error below in gerrit logs:
    
    "Failed to connect to 2610:28:3090:3001:5054:ff:feb9:6ae0: Network is
    unreachable" when trying to access fedorapeople.org.
    
    Change-Id: I96603948560bc7ea259235cf3b304b2e64ceb5ab

--- tests/setup_gate.sh
+++ tests/setup_gate.sh
@@ -11,4 +11,8 @@ group_str="jenkins ALL=(:dockerroot) NOPASSWD: ALL"
 sudo grep -x "$group_str" /etc/sudoers > /dev/null || sudo bash -c "echo \"$group_str\" >> /etc/sudoers"
 sudo chown root:dockerroot /var/run/docker.sock
 
+# disable ipv6 until we're sure routes to fedora mirrors work properly
+sudo sh -c 'echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf'
+sudo /usr/sbin/sysctl -p
+
 echo "Completed $0."
