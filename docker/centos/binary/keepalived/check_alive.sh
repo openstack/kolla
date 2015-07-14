02bd1332066eb0531046803f6b35282ebc9be02b
Martin André <martin.andre@kvhasia.com>
2015-07-14 14:42:18 +0900

    Move keepalived common files to common dir
    While not a problem in itself, all common files should be moved to
    docker/common/keepalived to be more consistent with other containers.
    
    Change-Id: I6176bbf30bf42c58d4480e7bf48b1c4625eb9a15
    Closes-Bug: #1474213

--- docker/centos/binary/keepalived/check_alive.sh
+++ docker/centos/binary/keepalived/check_alive.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-
-# This is noop check script. Change it to something meaningful.
-# TODO (inc0): When HAProxy is implemented, this should be changed to HAProxy check
-# https://github.com/stackforge/kolla/blob/master/specs/high-availability.rst
-
-exit 0
+../../../common/keepalived/check_alive.sh
---
+++ docker/common/keepalived/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. /opt/kolla/kolla-common.sh
+
+check_required_vars KEEPALIVED_HOST_PRIORITIES PUBLIC_IP PUBLIC_INTERFACE
+
+MY_HOSTNAME=`hostname`
+
+# here we unpack KEEPALIVED_HOST_PRIORITIES hostname:priority pairs and match
+# them with current hostname, if it's there
+for i in ${KEEPALIVED_HOST_PRIORITIES//,/ }; do
+    HOST_PRIORITY=(${i//:/ })
+    if [ "$MY_HOSTNAME" == "${HOST_PRIORITY[0]}" ]; then
+        KEEPALIVED_PRIORITY=${HOST_PRIORITY[1]}
+    fi
+done
+
+if [ -z "$KEEPALIVED_PRIORITY" ]; then
+    echo "ERROR: missing hostname in KEEPALIVED_HOST_PRIORITIES: $MY_HOSTNAME" >&2
+    exit 1
+fi
+
+sed -i '
+    s|@PUBLIC_INTERFACE@|'$PUBLIC_INTERFACE'|g
+    s|@PUBLIC_IP@|'$PUBLIC_IP'|g
+    s|@KEEPALIVED_PRIORITY@|'$KEEPALIVED_PRIORITY'|g
+' /etc/keepalived/keepalived.conf
+
+exec /usr/sbin/keepalived -nld -p /run/keepalived.pid
