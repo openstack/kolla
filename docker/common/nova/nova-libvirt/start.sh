3b30760e2d0bbc7864e54b5756be2bf2431a221d
Sam Yaple <sam@yaple.net>
2015-07-23 17:19:44 +0000

    Fix compute permissions and start params
    neutron-server was not properly using ml2_conf.ini
    
    nova did not have ovs utils, and it had incorrect libvirt permissions
    
    Change-Id: I43d2110d0b5fc1940d887dace95787e1ebbe142d
    Partitially-Implements: blueprint ansible-service

--- docker/common/nova/nova-libvirt/start.sh
+++ docker/common/nova/nova-libvirt/start.sh
@@ -10,4 +10,12 @@ source /opt/kolla/kolla-common.sh
 # Config-internal script exec out of this function, it does not return here.
 set_configs
 
+# TODO(SamYaple): Unify this with config-internal. Tweak libvirt.conf rather
+#                 than change permissions.
+# Fix permissions for libvirt
+if [[ -c /dev/kvm ]]; then
+    chmod 660 /dev/kvm
+    chown root:kvm /dev/kvm
+fi
+
 exec $CMD $ARGS
