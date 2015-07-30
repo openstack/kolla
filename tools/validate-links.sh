d4c7129dc30008f70ac07903aa27fdcd7b819321
Swapnil Kulkarni <me@coolsvap.net>
2015-07-30 06:54:41 +0000

    Remove not required echo
    - tools/validate-links.sh has a echo which
      is not necessary. Removed it.
    
    Change-Id: I122c995bc5d99592212505ce23256e27685b4af4
    Closes-Bug: #1479647

--- tools/validate-links.sh
+++ tools/validate-links.sh
@@ -3,7 +3,6 @@
 REAL_PATH=$(python -c "import os,sys;print os.path.realpath('$0')")
 cd "$(dirname "$REAL_PATH")/.."
 
-echo "Checking for broken links"
 if find . -type l -exec test ! -e {} \; -print | egrep '.*'; then
     exit 1
 fi
