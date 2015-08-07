65ed3c7f53ba19e8f221b293faa9021607c1f627
Sam Yaple <sam@yaple.net>
2015-08-07 18:48:27 +0000

    Remove config-internal for gnocchi
    Change-Id: Icf74913d03a780cd7c59fbc26a55344f2a12fdfa
    Partially-implements: blueprint remove-config-internal

--- docker/centos/binary/gnocchi/gnocchi-base/config-gnocchi.sh
+++
@@ -1,1 +0,0 @@
-../../../../common/gnocchi/gnocchi-base/config-gnocchi.sh
--- docker/common/gnocchi/gnocchi-statsd/config-internal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-. /opt/kolla/kolla-common.sh
-. /opt/kolla/config-gnocchi.sh
-
-exec /usr/bin/gnocchi-statsd
