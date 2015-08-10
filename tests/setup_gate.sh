4d85d9d0161620c462f9541ba7005b911fbb9566
Steven Dake <stdake@cisco.com>
2015-08-10 05:15:58 -0700

    Use docker-engine in setup_gate
    The fedora version of docker is hacked by Red Hat to have specific
    features which we don't want in the gating.  Use the pristine
    upstream version from Docker Inc.
    
    Change-Id: Ied219bc3868176f541083e9d42cde7d6cdeeb71d
    Partially-Implements: blueprint gate-source-builds

--- tests/setup_gate.sh
+++ tests/setup_gate.sh
@@ -2,14 +2,13 @@
 
 set -e
 
-sudo yum install -y libffi-devel openssl-devel docker
-sudo /usr/sbin/usermod -a -G dockerroot ${SUDO_USER:-$USER}
+sudo yum install -y libffi-devel openssl-devel
+sudo yum install -y http://yum.dockerproject.org/repo/main/fedora/21/Packages/docker-engine-1.7.1-1.fc21.x86_64.rpm
 sudo systemctl start docker
 sleep 1
 
-group_str="jenkins ALL=(:dockerroot) NOPASSWD: ALL"
+group_str="jenkins ALL=(:docker) NOPASSWD: ALL"
 sudo grep -x "$group_str" /etc/sudoers > /dev/null || sudo bash -c "echo \"$group_str\" >> /etc/sudoers"
-sudo chown root:dockerroot /var/run/docker.sock
 
 # disable ipv6 until we're sure routes to fedora mirrors work properly
 sudo sh -c 'echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf'
