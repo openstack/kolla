17333aed12775a439dc8d0699bf1811d7160ac55
Sam Yaple <sam@yaple.net>
2015-08-20 14:49:49 +0000

    Fix keystone for Ubuntu
    Due to the differences between Ubuntu and Centos around Apache2 I have
    had to copy both start.sh and config-external.sh to be unique files
    for Ubuntu so as not to affect Centos either. This can be removed when
    we change the way config-external works.
    
    Change-Id: I14dc11e7bb6284b897259a53acc914ae8f254d3c
    Partially-Implements: blueprint install-from-ubuntu

--- docker/common/keystone/config-external.sh
+++ docker/common/keystone/config-external.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 SOURCE="/opt/kolla/keystone/keystone.conf"
 TARGET="/etc/keystone/keystone.conf"
-SOURCE_WSGI="/opt/kolla/keystone/wsgi-keystone.conf"
-TARGET_WSGI="/etc/httpd/conf.d/wsgi-keystone.conf"
 OWNER="keystone"
 
 if [[ -f "$SOURCE" ]]; then
     cp $SOURCE $TARGET
     chown ${OWNER}: $TARGET
     chmod 0644 $TARGET
+fi
+
+if [[ "${KOLLA_BASE_DISTRO}" == "ubuntu" || \
+        "${KOLLA_BASE_DISTRO}" == "debian" ]]; then
+    SOURCE_WSGI="/opt/kolla/keystone/wsgi-keystone.conf"
+    TARGET_WSGI="/etc/apache2/conf-enabled/wsgi-keystone.conf"
+else
+    SOURCE_WSGI="/opt/kolla/keystone/wsgi-keystone.conf"
+    TARGET_WSGI="/etc/httpd/conf.d/wsgi-keystone.conf"
 fi
 
 if [[ -f "$SOURCE_WSGI" ]]; then
--- docker/common/keystone/start.sh
+++ docker/common/keystone/start.sh
@@ -2,11 +2,20 @@
 
 set -o errexit
 
-CMD="/usr/sbin/httpd"
-ARGS="-DFOREGROUND"
-
-# Loading common functions.
+# Loading common functions
 source /opt/kolla/kolla-common.sh
+
+if [[ "${KOLLA_BASE_DISTRO}" == "ubuntu" || \
+        "${KOLLA_BASE_DISTRO}" == "debian" ]]; then
+    CMD="/usr/sbin/apache2"
+    ARGS="-DFOREGROUND"
+
+    # Loading Apache2 ENV variables
+    source /etc/apache2/envvars
+else
+    CMD="/usr/sbin/httpd"
+    ARGS="-DFOREGROUND"
+fi
 
 # Execute config strategy
 set_configs
