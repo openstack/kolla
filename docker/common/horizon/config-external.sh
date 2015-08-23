6a297265c5b1a3fa6ef134e8953ea5c1bbfdca02
Sam Yaple <sam@yaple.net>
2015-08-23 12:01:35 +0000

    Ubuntu - Horizon
    Updates support for Ubuntu to install Horizon.
    
    Update the Horizon install block to follow the same structure as all
    of the other source install blocks.
    
    Removed local_settings copy since that will be copied in from outside
    with the config-external.sh script
    
    Change-Id: Iab386386286d66e6f74407a043e78cb5cb1b032b
    Partially-Implements: blueprint install-from-ubuntu

--- docker/common/horizon/config-external.sh
+++ docker/common/horizon/config-external.sh
@@ -8,3 +8,18 @@ if [[ -f "$SOURCE" ]]; then
     chown ${OWNER}: $TARGET
     chmod 0644 $TARGET
 fi
+
+if [[ "${KOLLA_BASE_DISTRO}" == "ubuntu" || \
+        "${KOLLA_BASE_DISTRO}" == "debian" ]]; then
+    SOURCE="/opt/kolla/horizon/horizon.conf"
+    TARGET="/etc/apache2/sites-enabled/000-default.conf"
+else
+    SOURCE="/opt/kolla/horizon/horizon.conf"
+    TARGET="/etc/httpd/sites.d/horizon.conf"
+fi
+
+if [[ -f "$SOURCE" ]]; then
+    cp $SOURCE $TARGET
+    chown ${OWNER}: $TARGET
+    chmod 0644 $TARGET
+fi
