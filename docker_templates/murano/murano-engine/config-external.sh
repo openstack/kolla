6434f19ef279f2575e9f0649e9940f88880da424
Steven Dake <stdake@cisco.com>
2015-08-27 18:27:37 -0700

    Move scripts for murano to docker_templates
    Change-Id: I1f7090d22c33891717002297c898cf1b5ddb210c
    Partially-Implements: blueprint remove-docker-dir

--- docker_templates/murano/murano-engine/config-external.sh
+++ docker_templates/murano/murano-engine/config-external.sh
@@ -1,1 +1,10 @@
-../../../docker/common/murano/murano-engine/config-external.sh
+#!/bin/bash
+SOURCE="/opt/kolla/murano/murano.conf"
+TARGET="/etc/murano/murano.conf"
+OWNER="murano"
+
+if [[ -f "$SOURCE" ]]; then
+    cp $SOURCE $TARGET
+    chown ${OWNER}: $TARGET
+    chmod 0644 $TARGET
+fi
--- docker_templates/murano/murano-engine/start.sh
+++ docker_templates/murano/murano-engine/start.sh
@@ -1,1 +1,13 @@
-../../../docker/common/murano/murano-engine/start.sh
+#!/bin/bash
+set -o errexit
+
+CMD="/usr/bin/murano-engine"
+ARGS="--config-file /etc/murano/murano.conf"
+
+# Loading common functions.
+source /opt/kolla/kolla-common.sh
+
+# Execute config strategy
+set_configs
+
+exec $CMD $ARGS
