bd32c7a2dc0b10b0be209f969b2f8b4ad87c55f7
Steven Dake <stdake@cisco.com>
2015-08-27 18:09:20 -0700

    Move scripts for horizon to docker_templates
    Change-Id: If260f47ea3e4f9917cf20a6e2013cc81bfa5fc83
    Partially-Implements: blueprint remove-docker-dir

--- docker_templates/horizon/config-external.sh
+++ docker_templates/horizon/config-external.sh
@@ -1,1 +1,25 @@
-../../docker/common/horizon/config-external.sh
+#!/bin/bash
+SOURCE="/opt/kolla/horizon/local_settings"
+TARGET="/etc/openstack-dashboard/local_settings"
+OWNER="horizon"
+
+if [[ -f "$SOURCE" ]]; then
+    cp $SOURCE $TARGET
+    chown ${OWNER}: $TARGET
+    chmod 0644 $TARGET
+fi
+
+if [[ "${KOLLA_BASE_DISTRO}" == "ubuntu" || \
+        "${KOLLA_BASE_DISTRO}" == "debian" ]]; then
+    SOURCE="/opt/kolla/horizon/horizon.conf"
+    TARGET="/etc/apache2/sites-enabled/000-default.conf"
+else
+    SOURCE="/opt/kolla/horizon/horizon.conf"
+    TARGET="/etc/httpd/sites.d/horizon.conf"
+fi
+
+if [[ -f "$SOURCE" ]]; then
+    cp $SOURCE $TARGET
+    chown ${OWNER}: $TARGET
+    chmod 0644 $TARGET
+fi
--- docker_templates/horizon/start.sh
+++ docker_templates/horizon/start.sh
@@ -1,1 +1,23 @@
-../../docker/common/horizon/start.sh
+#!/bin/bash
+
+set -o errexit
+
+# Loading common functions
+source /opt/kolla/kolla-common.sh
+
+if [[ "${KOLLA_BASE_DISTRO}" == "ubuntu" || \
+        "${KOLLA_BASE_DISTRO}" == "debian" ]]; then
+    CMD="/usr/sbin/apache2"
+    ARGS="-DFOREGROUND"
+
+    # Loading Apache2 ENV variables
+    source /etc/apache2/envvars
+else
+    CMD="/usr/sbin/httpd"
+    ARGS="-DFOREGROUND"
+fi
+
+# Execute config strategy
+set_configs
+
+exec $CMD $ARGS
