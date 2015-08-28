dc2869684afbdc715dc26b9feaf25f244ba6c1e8
Steven Dake <stdake@cisco.com>
2015-08-27 17:55:22 -0700

    Move scripts for galera to docker_templates
    Change-Id: I16639642d4e8bc5074d72d2f4b7fa1ec3a8f31de
    Partially-Implements: blueprint remove-docker-dir

--- docker_templates/galera/config-external.sh
+++ docker_templates/galera/config-external.sh
@@ -1,1 +1,11 @@
-../../docker/common/galera/config-external.sh
+#!/bin/bash
+SOURCE="/opt/kolla/mariadb/galera.cnf"
+TARGET="/etc/my.cnf.d/galera.cnf"
+OWNER="mysql"
+
+# Cluster configuration
+if [[ -f "$SOURCE" ]]; then
+    cp $SOURCE $TARGET
+    chown ${OWNER}: $TARGET
+    chmod 0600 $TARGET
+fi
