3ca9ae380ee20a2c9d374ea2ddc0dab2841d859a
Steven Dake <stdake@cisco.com>
2015-08-27 17:22:44 -0700

    Move kolla-common to bsae directory
    Change-Id: Ie48e1d25f06a2b91d608edda7fae54ec1ad80f2a
    Partially-Implements: blueprint remove-docker-dir

--- docker_templates/base/kolla-common.sh
+++ docker_templates/base/kolla-common.sh
@@ -1,1 +1,22 @@
-../../docker/common/base/kolla-common.sh
+#!/bin/bash
+
+set_configs() {
+    case $KOLLA_CONFIG_STRATEGY in
+        COPY_ALWAYS)
+            source /opt/kolla/config-external.sh
+            ;;
+        COPY_ONCE)
+            if [[ -f /configured ]]; then
+                echo 'INFO - This container has already been configured; Refusing to copy new configs'
+            else
+                source /opt/kolla/config-external.sh
+                touch /configured
+            fi
+            ;;
+
+        *)
+            echo '$KOLLA_CONFIG_STRATEGY is not set properly'
+            exit 1
+            ;;
+    esac
+}
