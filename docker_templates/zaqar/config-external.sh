5c82e0c90ce0fedf523b11b0b933f2276edf8f46
Steven Dake <stdake@cisco.com>
2015-08-27 19:09:28 -0700

    Move scripts for zaqar to docker_templates
    Change-Id: I558b4f1d8eceb420a3bf77297bd699f277f840b1
    Partially-Implements: blueprint remove-docker-dir

--- docker_templates/zaqar/config-external.sh
+++ docker_templates/zaqar/config-external.sh
@@ -1,1 +1,10 @@
-../../docker/common/zaqar/config-external.sh
+#!/bin/bash
+SOURCE="/opt/kolla/zaqar/zaqar.conf"
+TARGET="/etc/zaqar/zaqar.conf"
+OWNER="zaqar"
+
+if [[ -f "$SOURCE" ]]; then
+    cp $SOURCE $TARGET
+    chown ${OWNER}: $TARGET
+    chmod 0644 $TARGET
+fi
