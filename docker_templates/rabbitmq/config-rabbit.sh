eef32f0353d5cee9b4bb876767aab5c8298375eb
Steven Dake <stdake@cisco.com>
2015-08-27 19:01:57 -0700

    Move scripts for rabbitmq to docker_templates
    Change-Id: I6f06c067c18e4a3eafbed2b2796aa1cfe6c9b28c
    Partially-Implements: blueprint remove-docker-dir

--- docker_templates/rabbitmq/config-rabbit.sh
+++ docker_templates/rabbitmq/config-rabbit.sh
@@ -1,1 +1,7 @@
-../../docker/common/rabbitmq/config-rabbit.sh
+#!/bin/bash
+
+function set_rabbitmq_cookie {
+    echo "${RABBITMQ_CLUSTER_COOKIE}" > /var/lib/rabbitmq/.erlang.cookie
+    chown -R rabbitmq: /var/lib/rabbitmq
+    chmod 400 /var/lib/rabbitmq/.erlang.cookie
+}
