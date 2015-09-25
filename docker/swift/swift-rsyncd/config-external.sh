e2e0fd288f52b62bbd0fda75a79261c4c2b8b51e
Sam Yaple <sam@yaple.net>
2015-09-25 13:00:55 +0000

    Remove config-external.sh
    Removes config-external for all services that have been replaced in
    Ansible
    
    Change-Id: I839a14418638b977fbc1d02ba6839811b0f909ea
    Partially-Implements: blueprint replace-config-external

--- docker/swift/swift-rsyncd/config-external.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-OWNER="swift"
-
-if [[ -f "/opt/kolla/swift-rsyncd/rsyncd.conf" ]]; then
-    cp /opt/kolla/swift-rsyncd/rsyncd.conf /etc/rsyncd.conf
-    chown ${OWNER}: /etc/rsyncd.conf
-    chmod 0640 /etc/rsyncd.conf
-fi
