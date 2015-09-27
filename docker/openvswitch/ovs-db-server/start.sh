94ea43fa555fac20c15cdb4f4214af20913b6270
Sam Yaple <sam@yaple.net>
2015-09-27 12:37:23 +0000

    Replace config-external - openvswitch
    Change-Id: Ib05442c6ee146d2deb49fc235ba16fc7e0c3d6c8
    Partially-Implements: blueprint replace-config-external

--- docker/openvswitch/ovs-db-server/start.sh
+++ docker/openvswitch/ovs-db-server/start.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
-
 set -o errexit
-
-LOG_FILE="/var/log/openvswitch/ovsdb-server.log"
-DB_FILE="/etc/openvswitch/conf.db"
-UNIXSOCK_DIR="/var/run/openvswitch"
-UNIXSOCK="${UNIXSOCK_DIR}/db.sock"
-
-CMD="/usr/sbin/ovsdb-server"
-ARGS="$DB_FILE -vconsole:emer -vsyslog:err -vfile:info --remote=punix:${UNIXSOCK} --log-file=${LOG_FILE}"
 
 # Loading common functions.
 source /opt/kolla/kolla-common.sh
 
-# Execute config strategy
-set_configs
+python /opt/kolla/set_configs.py
+CMD=$(cat /run_command)
 
-mkdir -p "${UNIXSOCK_DIR}"
-
-if [[ ! -e "${DB_FILE}" ]]; then
-    ovsdb-tool create "${DB_FILE}"
+mkdir -p "/run/openvswitch"
+if [[ ! -e "/etc/openvswitch/conf.db" ]]; then
+    ovsdb-tool create "/etc/openvswitch/conf.db"
 fi
 
-exec $CMD $ARGS
+exec $CMD
