a4467331cbdd8cbac813ff17c177229a8e3b8392
Sam Yaple <sam@yaple.net>
2015-10-13 06:30:30 +0000

    Fix issue with su and bad kernels
    Long story short, some kernels before 3.15 had an issue with using su
    in a container when the network namespace was --net=host. The gate
    has a 3.10 and a 3.13 kernel and has a problem with this. This changes
    everything to use sudo
    
    backport: liberty
    Partially-Implements: blueprint functional-testing-gate
    Change-Id: I4d79ccaa1cddffcc8393f64e7e1be2538efe33e5
    (cherry picked from commit 5f200e044648efe64f297d186432839762f91395)

--- docker/magnum/magnum-api/extend_start.sh
+++ docker/magnum/magnum-api/extend_start.sh
@@ -3,6 +3,6 @@
 # Bootstrap and exit if KOLLA_BOOTSTRAP variable is set. This catches all cases
 # of the KOLLA_BOOTSTRAP variable being set, including empty.
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
-    su -s /bin/sh -c "magnum-manage db_sync" magnum
+    sudo -H -u magnum magnum-manage db_sync
     exit 0
 fi
