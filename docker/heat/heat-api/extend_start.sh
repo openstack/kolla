64b767f9f659ac6dd3b2464967ff0c4d24844761
Michal Rostecki <mrostecki@mirantis.com>
2015-10-17 09:50:03 +0200

    Use trusts in heat.conf
    Change-Id: Ie91b3498e9f9d0d74a502ba6945db531ef967516
    Closes-Bug: #1492736

--- docker/heat/heat-api/extend_start.sh
+++ docker/heat/heat-api/extend_start.sh
@@ -4,8 +4,10 @@
 # of the KOLLA_BOOTSTRAP variable being set, including empty.
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     heat-manage db_sync
-    openstack domain create heat_user_domain
-    openstack user create --domain heat_user_domain heat_domain_admin --password ${HEAT_DOMAIN_ADMIN_PASSWORD}
-    openstack role add --domain heat_user_domain --user heat_domain_admin admin
+    openstack domain create heat
+    openstack user create --domain heat heat_domain_admin --password ${HEAT_DOMAIN_ADMIN_PASSWORD}
+    openstack role add --domain heat --user heat_domain_admin admin
+    openstack role create heat_stack_owner
+    openstack role create heat_stack_user
     exit 0
 fi
