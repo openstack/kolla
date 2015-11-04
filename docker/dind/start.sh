6a9beb4a33a0a44ba7ffbafc80d794604f030d57
Sam Yaple <sam@yaple.net>
2015-11-04 02:32:11 +0000

    Add Docker in Docker container
    Change-Id: I3c953125ed0105b7e8b62e62da56bf3fa30889d1
    Partially-Implements: blueprint multinode-gate

---
+++ docker/dind/start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+# External processes will be occuring and we must wait for them
+while [[ ! -e "/kolla_dind_ready" ]]; do
+    sleep 1
+done
+
+docker daemon -s btrfs 2>&1 > docker.log &
+docker_pid=$!
+
+mkdir /var/run/sshd
+/usr/sbin/sshd -D 2>&1 > sshd.log &
+sshd_pid=$!
+
+mkdir -p /root/.ssh/
+echo "${SSH_PUB}" > /root/.ssh/authorized_keys
+
+# Wait for docker daemon
+sleep 5
+
+# Due to a quirk in the cloning method we end up with a bunch of dead containers
+docker rm -v -f $(docker ps -a --no-trunc -q)
+
+# Wait until child processes exit (they should never exit)
+wait
