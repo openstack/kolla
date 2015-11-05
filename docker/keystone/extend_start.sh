d30fb21f8185a042d12a2a9d0b76ebef4a8733cc
Paul Bourke <paul.bourke@oracle.com>
2015-11-05 11:46:14 +0000

    Clean up keystone httpd pid files for RPM distros
    In some cases we're seeing httpd not cleaning up properly after itself,
    which results in the keystone container failing to restart. This is
    confirmed to happen on rpm based distros, but have not had any reports
    on Ubuntu.
    
    Change-Id: I58b006189e700f1c851601b4f64dd0fae931103c
    Closes-Bug: #1489676
    Co-Authored-By: Tim Potter <tpot@hpe.com>
    (cherry picked from commit 7df3a0bea56f654a3d65da27c0e783f775b4ed18)

--- docker/keystone/extend_start.sh
+++ docker/keystone/extend_start.sh
@@ -6,6 +6,13 @@ if [[ "${KOLLA_BASE_DISTRO}" == "ubuntu" || \
     source /etc/apache2/envvars
 fi
 
+# NOTE(pbourke): httpd will not clean up after itself in some cases which
+# results in the container not being able to restart. Unconfirmed if this
+# happens on Ubuntu. (bug #1489676)
+if [[ "${KOLLA_BASE_DISTRO}" =~ fedora|centos|oraclelinux|rhel ]]; then
+    rm -rf /var/run/httpd/* /run/httpd/* /tmp/httpd*
+fi
+
 # Bootstrap and exit if KOLLA_BOOTSTRAP variable is set. This catches all cases
 # of the KOLLA_BOOTSTRAP variable being set, including empty.
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
