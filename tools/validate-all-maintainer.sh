33d39f29421bec986b96b112d11cc8caa36a896d
Angus Salkeld <asalkeld@mirantis.com>
2015-11-09 15:20:13 +1000

    Fix validate-all-maintainer.sh to look for Dockerfile.j2
    And fix the fallout.
    
    Change-Id: Iccad3f4fdb0a6a7c14246df3408cae0425b833a5

--- tools/validate-all-maintainer.sh
+++ tools/validate-all-maintainer.sh
@@ -3,6 +3,5 @@
 REAL_PATH=$(python -c "import os,sys;print os.path.realpath('$0')")
 cd "$(dirname "$REAL_PATH")/.."
 
-find docker -name Dockerfile -print0 |
+find docker -name Dockerfile.j2 -print0 |
     xargs -0 tools/validate-maintainer.sh || exit 1
-
