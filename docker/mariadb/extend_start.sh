09e9b1be33b0f0b3ee75f9545e6430877b6e0264
Steven Dake <stdake@cisco.com>
2015-11-10 04:15:34 -0500

    Move the mariadb expect code to a script
    Atleast in a script, sudo can be made to only allow the script to
    run from the mysql process in the future, versus all the proceesses
    being able to be executed as root presently.
    
    Change-Id: I030b57086e37e4dc8f668f98c04335d94ab9d2b0
    Partially-Implements: blueprint drop-root

--- docker/mariadb/extend_start.sh
+++ docker/mariadb/extend_start.sh
@@ -5,26 +5,7 @@ function bootstrap_db {
 
     # Waiting for deamon
     sleep 10
-    expect -c '
-    set timeout 10
-    spawn mysql_secure_installation
-    expect "Enter current password for root (enter for none):"
-    send "\r"
-    expect "Set root password?"
-    send "y\r"
-    expect "New password:"
-    send "'"${DB_ROOT_PASSWORD}"'\r"
-    expect "Re-enter new password:"
-    send "'"${DB_ROOT_PASSWORD}"'\r"
-    expect "Remove anonymous users?"
-    send "y\r"
-    expect "Disallow root login remotely?"
-    send "n\r"
-    expect "Remove test database and access to it?"
-    send "y\r"
-    expect "Reload privilege tables now?"
-    send "y\r"
-    expect eof'
+    kolla_security_reset
 
     mysql -u root --password="${DB_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}' WITH GRANT OPTION;"
     mysql -u root --password="${DB_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${DB_ROOT_PASSWORD}' WITH GRANT OPTION;"
