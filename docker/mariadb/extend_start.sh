4c9e15b94e3f8e27a3ef194768c64357efa9ea82
Steven Dake <stdake@cisco.com>
2015-11-10 04:11:20 -0500

    Drop root privileges for mariadb
    Drop root privileges for mariadb.  This isn't perfect.  If somemone
    breaks out of the container and can run sudo within the contianer,
    it would be possible to replace the root credentials of the database.
    
    Any container that uses sudo suffers from some extra attack vector
    related to the sudo command.  That said, the sudo commands are
    locked down to minimize harm.
    
    Change-Id: I4b3573725d940bb8aa90d43a6235d8cf7d30fc64
    Partially-Implements: blueprint drop-root

--- docker/mariadb/extend_start.sh
+++ docker/mariadb/extend_start.sh
@@ -5,19 +5,19 @@ function bootstrap_db {
 
     # Waiting for deamon
     sleep 10
-    kolla_security_reset
+    sudo -E kolla_security_reset
 
     mysql -u root --password="${DB_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}' WITH GRANT OPTION;"
     mysql -u root --password="${DB_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${DB_ROOT_PASSWORD}' WITH GRANT OPTION;"
-    mysqladmin -p"${DB_ROOT_PASSWORD}" shutdown
+    mysqladmin -uroot -p"${DB_ROOT_PASSWORD}" shutdown
 }
 
-chown mysql: /var/lib/mysql
+sudo chown mysql: /var/lib/mysql
 
 # This catches all cases of the BOOTSTRAP variable being set, including empty
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]] && [[ ! -e /var/lib/mysql/cluster.exists ]]; then
     ARGS="--wsrep-new-cluster"
     touch /var/lib/mysql/cluster.exists
-    mysql_install_db --user=mysql
+    mysql_install_db
     bootstrap_db
 fi
