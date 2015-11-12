22def41d37d8046541b25fca1fe806090bf14638
Ryan Hallisey <rhallise@redhat.com>
2015-11-12 10:46:10 -0500

    Drop root privileges for rabbitmq
    Drop root privileges for rabbitmq.  Only the rabbitmq user
    will be able to execute chown of /var/lib/rabbitmq.
    
    Change-Id: I546e6b475a8462bfbc75972854e1fee64f96d9cb
    Partially-Implements: blueprint drop-root

--- docker/rabbitmq/extend_start.sh
+++ docker/rabbitmq/extend_start.sh
@@ -3,8 +3,8 @@
 # Bootstrap and exit if KOLLA_BOOTSTRAP variable is set. This catches all cases
 # of the KOLLA_BOOTSTRAP variable being set, including empty.
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
+    sudo chown -R rabbitmq: /var/lib/rabbitmq
     echo "${RABBITMQ_CLUSTER_COOKIE}" > /var/lib/rabbitmq/.erlang.cookie
-    chown -R rabbitmq: /var/lib/rabbitmq
     chmod 400 /var/lib/rabbitmq/.erlang.cookie
     exit 0
 fi
