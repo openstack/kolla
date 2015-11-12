8f9c42758a3df46b788cbfcfa8cc40faeb7d43e3
Sam Yaple <sam@yaple.net>
2015-11-12 17:05:31 +0000

    Fix namespace regression for neutron
    Due the to way network namespaces work, they will not survive a
    container restart. This is not an issue related to Kolla or Neutron
    but rather an issue with how network namespaces work.
    
    This is putting back into place a workaround that was in correctly
    removed in the past. We simply cleanup the namespace cruft when the
    neutron-agents container restarts thus avoiding this issue.
    
    Change-Id: Ic16f2f766b4fa46347ea14a1ece5a4653c69d779
    Closes-Bug: #1511216
    (cherry picked from commit baf9d93358f8fc79df9bb71f24dbc50b85860ea1)

--- docker/neutron/neutron-agents/start.sh
+++ docker/neutron/neutron-agents/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -o errexit
 
+# We must remove all of the stale namespaces if they exist
+rm -f /run/netns/*
+
 # Processing /var/lib/kolla/config_files/config.json
 python /usr/local/bin/kolla_set_configs
 
