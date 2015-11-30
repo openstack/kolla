2a491b9896ac68314429387517d737f426f2d042
Swapnil Kulkarni (coolsvap) <me@coolsvap.net>
2015-11-30 11:35:18 +0530

    Drop root for murano
    Updates to ensure commands run in the murano containers
    are done as the 'murano' user rather than root.
    
    Change-Id: I558c46b032f11b83a9dc6b5abc158aa6d24ec0eb
    Partially-Implements: blueprint drop-root

--- docker/murano/murano-api/extend_start.sh
+++ docker/murano/murano-api/extend_start.sh
@@ -3,6 +3,6 @@
 # Bootstrap and exit if KOLLA_BOOTSTRAP variable is set. This catches all cases
 # of the KOLLA_BOOTSTRAP variable being set, including empty.
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
-    sudo -H -u murano murano-db-manage --config-file /etc/murano/murano.conf upgrade
+    murano-db-manage --config-file /etc/murano/murano.conf upgrade
     exit 0
 fi
