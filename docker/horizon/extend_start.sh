4afe624f07c7184aaf8c2a7deba5f61b9342ed32
Jeffrey Zhang <zhang.lei.fly@gmail.com>
2016-01-26 00:27:58 +0800

    Re-generate the compressed css and js on every horizon start
    The local_settings file affect django-compress behavior, so
    re-generate the compressed javascript and css if it is changed
    
    Closes-Bug: #1537815
    Change-Id: I330bfb666e39ffb88a4488793562df697d5572d0

--- docker/horizon/extend_start.sh
+++ docker/horizon/extend_start.sh
@@ -12,3 +12,16 @@ fi
 if [[ "${KOLLA_BASE_DISTRO}" =~ fedora|centos|oraclelinux|rhel ]]; then
     rm -rf /var/run/httpd/* /run/httpd/* /tmp/httpd*
 fi
+
+# NOTE(jeffrey4l): The local_settings file affect django-compress
+# behavior, so re-generate the compressed javascript and css if it
+# is changed
+MD5SUM_TXT_PATH="/home/horizon/.local_settings.md5sum.txt"
+if [[ ! -f ${MD5SUM_TXT_PATH} || $(md5sum -c --status ${MD5SUM_TXT_PATH};echo $?) != 0 ]]; then
+    md5sum /etc/openstack-dashboard/local_settings > ${MD5SUM_TXT_PATH}
+    if [[ "${KOLLA_INSTALL_TYPE}" == "binary" ]]; then
+        /usr/bin/manage.py compress --force
+    elif [[ "${KOLLA_INSTALL_TYPE}" == "source" ]]; then
+        /var/lib/kolla/venv/bin/python /var/lib/kolla/venv/bin/manage.py compress --force
+    fi
+fi
