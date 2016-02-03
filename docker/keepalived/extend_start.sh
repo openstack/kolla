3fca4ccbaf66eedc6035826afd842cde02049ba7
Éric Lemoine <elemoine@mirantis.com>
2016-02-03 11:55:14 +0100

    Make Heka collect HAProxy and Keepalived logs
    Partially implements: blueprint heka
    Change-Id: I7e2bf4e520fa14fd40e3b329f3b2998ae6ea47f4

--- docker/keepalived/extend_start.sh
+++ docker/keepalived/extend_start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# NOTE(elemoine): keepalived cannot be configured to change the log address to
+# anything other than /dev/log. Heka's log socket is at /var/lib/kolla/heka/log
+# so we symlink /dev/log to that location.
+ln -sf /var/lib/kolla/heka/log /dev/log
+
 modprobe ip_vs
 
 # Workaround for bug #1485079
