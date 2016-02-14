2c45bef06573f31513fc90f9984b4c413fbb1bef
SamYaple <sam@yaple.net>
2016-02-14 23:36:52 +0000

    Reduce gate resource usage
    Change-Id: Ic6cd45ec708efe4f573e19f3bc910717ac3b4fbb
    Partially-Implements: blueprint functional-testing-gate

--- tools/deploy_aio.sh
+++ tools/deploy_aio.sh
@@ -5,6 +5,9 @@ set -o errexit
 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
+export KOLLA_BASE=$1
+export KOLLA_TYPE=$2
+
 function check_failure {
     docker ps -a
     failed_containers=$(docker ps -a --format "{{.Names}}" --filter status=exited)
@@ -13,24 +16,53 @@ function check_failure {
         docker logs --tail all ${failed}
     done
 
+    journalctl --no-pager -u docker.service || :
+    cat /var/log/upstart/docker.log || :
+
     if [[ -n ${failed_containers} ]]; then
         echo 'FAILED'
         exit 1
     fi
 }
 
-trap check_failure EXIT
+function write_configs {
+    mkdir -p /etc/kolla/config
 
-# Populate globals.yml
-cat << EOF > /etc/kolla/globals.yml
+    cat << EOF > /etc/kolla/globals.yml
 ---
-kolla_base_distro: "$1"
-kolla_install_type: "$2"
+kolla_base_distro: "${KOLLA_BASE}"
+kolla_install_type: "${KOLLA_TYPE}"
 kolla_internal_address: "169.254.169.10"
 docker_restart_policy: "never"
 network_interface: "eth0"
 neutron_external_interface: "fake_interface"
+enable_horizon: "no"
+enable_heat: "no"
 EOF
+
+    cat << EOF > /etc/kolla/config/nova.conf
+[DEFAULT]
+osapi_compute_workers = 1
+
+[conductor]
+workers = 1
+EOF
+
+    cat << EOF > /etc/kolla/config/glance.conf
+[DEFAULT]
+workers = 1
+EOF
+
+    cat << EOF > /etc/kolla/config/neutron.conf
+[DEFAULT]
+api_workers = 1
+metadata_workers = 1
+EOF
+}
+
+trap check_failure EXIT
+
+write_configs
 
 # Create dummy interface for neutron
 ip l a fake_interface type dummy
