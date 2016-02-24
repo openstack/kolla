1382e39f162d827199a8baa39cffb9c1812cc3c1
Éric Lemoine <elemoine@mirantis.com>
2016-02-24 11:48:06 +0100

    Remove duplicate code in cinder extend_start.sh
    Change-Id: I71953c905b978176bf7dc6bcaf3c4766be4b60fb
    Partial-Bug: #1546944

--- docker/cinder/cinder-api/extend_start.sh
+++ docker/cinder/cinder-api/extend_start.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -o errexit
 
-if [[ ! -d "/var/log/kolla/cinder" ]]; then
-    mkdir -p /var/log/kolla/cinder
-fi
-if [[ $(stat -c %a /var/log/kolla/cinder) != "755" ]]; then
-    chmod 755 /var/log/kolla/cinder
-fi
-
 # Bootstrap and exit if KOLLA_BOOTSTRAP variable is set. This catches all cases
 # of the KOLLA_BOOTSTRAP variable being set, including empty.
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
