d5d1b1b3ddb3802e992b39a9df383dfc5dd9753d
SamYaple <sam@yaple.net>
2016-03-02 19:38:41 +0000

    heatclient has invalid json validation picks up
    We shouldn't be scanning .tox dir anyway.
    
    TrivialFix
    
    Change-Id: If570abdfa69375091751199dcce637682e8de1ef

--- tools/validate-all-json.sh
+++ tools/validate-all-json.sh
@@ -3,6 +3,6 @@
 REAL_PATH=$(python -c "import os,sys;print os.path.realpath('$0')")
 cd "$(dirname "$REAL_PATH")/.."
 
-find . -name '*.json' -print0 |
+find . -path ./.tox -prune -name '*.json' -print0 |
     xargs -0 python tools/validate-json.py || exit 1
 
