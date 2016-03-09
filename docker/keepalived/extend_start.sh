500dc73c87f54052a7f632ecd5eeefa3a86f7f49
Éric Lemoine <elemoine@mirantis.com>
2016-03-09 10:38:34 +0100

    Do not create /dev/log symlink if it already exists
    This changes the keepalived extend_start.sh script not to recreate
    the /dev/log symlink if it already exists.
    
    This change is related to a comment from SamYaple in
    https://review.openstack.org/#/c/288432/.
    
    TrivialFix
    
    Change-Id: I9ca2b4d0b36c961ab3e1e18e2011dd10b47c33e3

--- docker/keepalived/extend_start.sh
+++ docker/keepalived/extend_start.sh
@@ -3,7 +3,9 @@
 # NOTE(elemoine): keepalived cannot be configured to change the log address to
 # anything other than /dev/log. Heka's log socket is at /var/lib/kolla/heka/log
 # so we symlink /dev/log to that location.
-ln -sf /var/lib/kolla/heka/log /dev/log
+if [[ ! -h /dev/log ]]; then
+    ln -sf /var/lib/kolla/heka/log /dev/log
+fi
 
 modprobe ip_vs
 
