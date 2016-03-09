21ecd9925aaee7ae4193d854daa1634bf60a27a5
Steven Dake <stdake@cisco.com>
2016-03-09 02:17:19 -0500

    Upgrade Heat
    This could possibly use gold plating in how bootstrapping
    operates to use shade rather than the shell in the container.
    It is unclear why stack create failed prior to this patch, but
    that fact hasn't changed.  I think the heat domains are not setup
    properly in the configuration files, meaning that the domain ID
    must be created outside of Heat and registered in the configuration
    file.  This is covered in Bug #1553565.
    
    Change-Id: I490d4dd68a101f388c0ecb4acab54d5eaa6e314e
    Partially-Implements: blueprint kolla-upgrade
    Implements: blueprint upgrade-heat

--- docker/heat/heat-api/extend_start.sh
+++ docker/heat/heat-api/extend_start.sh
@@ -4,10 +4,14 @@
 # of the KOLLA_BOOTSTRAP variable being set, including empty.
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     heat-manage db_sync
-    openstack domain create heat
-    openstack user create --domain heat heat_domain_admin --password ${HEAT_DOMAIN_ADMIN_PASSWORD}
-    openstack role add --domain heat --user heat_domain_admin admin
-    openstack role create heat_stack_owner
-    openstack role create heat_stack_user
+    CURRENT_HEAT_DOMAIN_NAME=$(openstack domain list | grep heat | awk '{print $4}')
+
+    if [[ "heat" != "$CURRENT_HEAT_DOMAIN_NAME" ]]; then
+        openstack domain create heat
+        openstack user create --domain heat heat_domain_admin --password ${HEAT_DOMAIN_ADMIN_PASSWORD}
+        openstack role add --domain heat --user heat_domain_admin admin
+        openstack role create heat_stack_owner
+        openstack role create heat_stack_user
+    fi
     exit 0
 fi
