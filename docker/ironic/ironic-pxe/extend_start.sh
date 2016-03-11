6345313d17349615a9c62bec279f036d8630d0b8
Andrey Shestakov <ashestakov@mirantis.com>
2016-03-11 16:10:28 +0200

    Add missing files in ironic containers
    Added missing package for qemu-img in centos container
    Added pxelinux.0 in ironic-pxe in centos container
    
    TrivialFix
    
    Change-Id: I706284824f915caf974b1247520c3e4025c47f57

--- docker/ironic/ironic-pxe/extend_start.sh
+++ docker/ironic/ironic-pxe/extend_start.sh
@@ -4,7 +4,7 @@
 # of the KOLLA_BOOTSTRAP variable being set, including empty.
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     chown -R ironic: /tftpboot
-    for pxe_file in /var/lib/tftpboot/chain.c32 /usr/lib/syslinux/pxelinux.0 /usr/lib/syslinux/chain.c32; do
+    for pxe_file in /var/lib/tftpboot/pxelinux.0 /var/lib/tftpboot/chain.c32 /usr/lib/syslinux/pxelinux.0 /usr/lib/syslinux/chain.c32; do
         if [[ -e "$pxe_file" ]]; then
             cp "$pxe_file" /tftpboot
         fi
