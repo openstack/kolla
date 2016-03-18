de6650874da1470f10d4561fd1e33829eaef3414
Jeffrey Zhang <jeffrey.zhang@99cloud.net>
2016-03-19 00:24:44 +0800

    Start the haproxy by using haproxy-system-wrapper
    haproxy-system-wrapper is a solution for systemd from upstream. it can
    handle the reload graceful.
    
    Change-Id: I6a3d141af065e429bd1be1b7252f5c6df1fda3bb
    Closes-Bug: #1559238

--- docker/haproxy/ensure_latest_config.sh
+++ docker/haproxy/ensure_latest_config.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -o errexit
+
 CURRENT_CONFIG_HASH=$(sha1sum /etc/haproxy/haproxy.cfg | cut -f1 -d' ')
 NEW_CONFIG_HASH=$(sha1sum /var/lib/kolla/config_files/haproxy.cfg | cut -f1 -d' ')
 
 if [[ $CURRENT_CONFIG_HASH != $NEW_CONFIG_HASH ]]; then
     changed=changed
     python /usr/local/bin/kolla_set_configs
-    /usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -p /run/haproxy.pid -sf $(cat /run/haproxy.pid)
+    kill -USR2 $(pgrep -f /usr/sbin/haproxy-systemd-wrapper)
 fi
 
 echo $changed
