28cc39e2fee14d58831e49458a66d6da0a4cd406
SamYaple <sam@yaple.net>
2016-03-21 18:56:52 +0000

    Fix bad apache2 close
    Change-Id: Id19450a566da1bf7b824f0050e7c3e3f73581a9b
    Closes-Bug: #1557036

--- docker/keystone/extend_start.sh
+++ docker/keystone/extend_start.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-if [[ "${KOLLA_BASE_DISTRO}" == "ubuntu" || \
-        "${KOLLA_BASE_DISTRO}" == "debian" ]]; then
+# NOTE(pbourke): httpd will not clean up after itself in some cases which
+# results in the container not being able to restart. (bug #1489676, 1557036)
+if [[ "${KOLLA_BASE_DISTRO}" =~ debian|ubuntu ]]; then
     # Loading Apache2 ENV variables
     source /etc/apache2/envvars
+    rm -rf /var/run/apache2/*
     APACHE_DIR="apache2"
 else
-    APACHE_DIR="httpd"
-fi
-
-# NOTE(pbourke): httpd will not clean up after itself in some cases which
-# results in the container not being able to restart. Unconfirmed if this
-# happens on Ubuntu. (bug #1489676)
-if [[ "${KOLLA_BASE_DISTRO}" =~ fedora|centos|oraclelinux|rhel ]]; then
     rm -rf /var/run/httpd/* /run/httpd/* /tmp/httpd*
+    APACHE_DIR="httpd"
 fi
 
 # Create log dir for Keystone logs
