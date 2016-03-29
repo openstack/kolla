4108c6c6ec0c0d31d9314a5d3129e4d4eff44190
Hui Kang <kangh@us.ibm.com>
2016-03-29 13:25:43 -0400

    Implement ceilometer ansible role
    Change-Id: I13c50a78b2dc7c98b720a4b2e7161104213bd295
    Implements: bp ansible-ceilometer

--- docker/ceilometer/ceilometer-api/extend_start.sh
+++ docker/ceilometer/ceilometer-api/extend_start.sh
@@ -3,7 +3,7 @@
 # Bootstrap and exit if KOLLA_BOOTSTRAP variable is set. This catches all cases
 # of the KOLLA_BOOTSTRAP variable being set, including empty.
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
-    ceilometer-manage db_sync
+    ceilometer-dbsync
     sudo chown -R ceilometer: /var/lib/ceilometer/
     exit 0
 fi
