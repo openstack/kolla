eb1bfc2c2e157e90a57f2a0f5186db0dea4c3909
Swapnil Kulkarni (coolsvap) <me@coolsvap.net>
2016-05-17 15:39:25 +0530

    Add support to lint the install commands in Dockerfiles
    Change-Id: Icb47ba8b13b13889e28bb285321f11344400edc2
    Closes-Bug:#1582618

--- tools/validate-all-dockerfiles.sh
+++ tools/validate-all-dockerfiles.sh
@@ -5,3 +5,7 @@ cd "$(dirname "$REAL_PATH")/.."
 
 find docker -name Dockerfile.j2 -print0 |
     xargs -0 tools/validate-maintainer.sh || exit 1
+
+find docker -name Dockerfile.j2 -print0 |
+    xargs -0 tools/validate-install-command.sh || exit 1
+
