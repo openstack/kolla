18ae114e7557ecc0a5692ea3a22b418df7bba52f
Will Miller <wmiller@cray.com>
2016-05-25 04:04:52 -0500

    Fix kolla-build return codes
    Add a main wrapper function to return 1 if any images failed to build;
    otherwise, return 0. Rename the old main() to run_build(), which still
    returns image statuses, to avoid interfering with unit tests.
    
    Change-Id: I5817cc9cebb4f42dc13e0535223e5dc8931a3aab
    Co-Authored-By: Mark Goddard <mgoddard@cray.com>
    Closes-Bug: #1578404

--- tests/test_build.py
+++ tests/test_build.py
@@ -43,7 +43,7 @@ class BuildTest(object):
     def runTest(self):
         with patch.object(sys, 'argv', self.build_args):
             LOG.info("Running with args %s", self.build_args)
-            bad_results, good_results, unmatched_results = build.main()
+            bad_results, good_results, unmatched_results = build.run_build()
 
         failures = 0
         for image, result in six.iteritems(bad_results):
