d360d4e5af09c5c194db783c4344aef10367b7f3
Ryan Hallisey <rhallise@redhat.com>
2016-06-13 07:01:48 -0400

    Change the search path to look locally
    In order to use tools/build.py, we need to search
    locally for imports.
    
    Closes-bug: #1592030
    Change-Id: Idfa651c1268f93366de9f4e3fa80c33be42c71c3

--- kolla/cmd/build.py
+++ kolla/cmd/build.py
@@ -12,6 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+import sys
+
+PROJECT_ROOT = os.path.abspath(os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), '../..'))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
 from kolla.image import build
 
 
