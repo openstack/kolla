54919dc4984121fcbc1de8f4f7ace00c37981a35
Hui Kang <kangh@us.ibm.com>
2016-06-26 23:45:33 -0400

    Enable openvswitch container logs in host volumes
    Allow heka container to pick up the openvswitch logs like other
    services
    
    Change-Id: I396ae9419540070c6ab01b8f62607ab46b1f08aa
    closes-Bug: #1573199

---
+++ docker/openvswitch/openvswitch-base/extend_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ ! -d "/var/log/kolla/openvswitch" ]]; then
+    mkdir -p /var/log/kolla/openvswitch
+fi
+if [[ $(stat -c %a /var/log/kolla/openvswitch) != "755" ]]; then
+    chmod 755 /var/log/kolla/openvswitch
+fi
+
+source /usr/local/bin/kolla_openvswitch_extend_start
