e4259f805b91ce0859e7190f97f8dbf34ee92e45
Ross Krumbeck <ross.krumbeck@gmail.com>
2016-08-08 20:14:43 +1000

    Fix designate bootstrap.
    According to
    http://docs.openstack.org/developer/designate/getting-started.html#initialize-start-the-central-service
    designate database bootstrap to be done by designate-manage database sync.
    Was also missing designate-manage pool_manager_cache sync.
    designate-manage command is packaged with designate-central.
    Boot strap commands moved to designate-central.
    Added creation of log directories.
    Added python2-oslo-reports and python2-suds for bootstrap to complete.
    
    Change-Id: I3896064b0abb31c96307b16005a340fc193aac3c
    Closes-Bug: 1610795

---
+++ docker/designate/designate-base/extend_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ ! -d "/var/log/kolla/designate" ]]; then
+    mkdir -p /var/log/kolla/designate
+fi
+if [[ $(stat -c %a /var/log/kolla/designate) != "755" ]]; then
+    chmod 755 /var/log/kolla/designate
+fi
+
+source /usr/local/bin/kolla_designate_extend_start
---
+++ docker/designate/designate-central/extend_start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ ! -f "/var/log/kolla/designate/designate-manage.log" ]]; then
+    touch /var/log/kolla/designate/designate-manage.log
+    chmod 644 /var/log/kolla/designate/designate-manage.log
+fi
+
+# Bootstrap and exit if KOLLA_BOOTSTRAP variable is set. This catches all cases
+# of the KOLLA_BOOTSTRAP variable being set, including empty.
+if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
+    designate-manage database sync
+    designate-manage pool-manager-cache sync
+    exit 0
+fi
