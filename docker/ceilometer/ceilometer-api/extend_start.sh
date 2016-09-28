18fa72c901434c023ab68754a2e68d726484f608
Jeffrey Zhang <zhang.lei.fly@gmail.com>
2016-09-29 01:14:55 +0800

    integrate gnocchi with ceilometer
    Closes-Bug: #1626364
    Change-Id: I9d586b950b7099a9b160f7b32c9ff00b189a0287

--- docker/ceilometer/ceilometer-api/extend_start.sh
+++ docker/ceilometer/ceilometer-api/extend_start.sh
@@ -3,7 +3,16 @@
 # Bootstrap and exit if KOLLA_BOOTSTRAP variable is set. This catches all cases
 # of the KOLLA_BOOTSTRAP variable being set, including empty.
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
-    sudo -H -u ceilometer ceilometer-upgrade --skip-gnocchi-resource-types
+    if [[ "${CEILOMETER_DATABASE_TYPE}" == "mysql" ]]; then
+        sudo -H -u ceilometer ceilometer-upgrade --skip-gnocchi-resource-types
+    elif [[ "${CEILOMETER_DATABASE_TYPE}" == "gnocchi" ]]; then
+        sudo -H -u ceilometer ceilometer-upgrade --skip-metering-database --skip-event-database
+    elif [[ "${CEILOMETER_DATABASE_TYPE}" == "mongodb" ]]; then
+        echo "Ceilometer doesn't need to initialize a database when MongoDB is configured as the database backend."
+    else
+        echo "Unsupported database type: ${CEILOMETER_DATABASE_TYPE}"
+        exit 1
+    fi
     sudo chown -R ceilometer: /var/lib/ceilometer/
     exit 0
 fi
