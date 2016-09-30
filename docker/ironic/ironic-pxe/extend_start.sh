06422a29c9435d932c63365d3b7c68977851c01a
Waldemar Znoinski <waldemar.znoinski@intel.com>
2016-09-30 14:12:04 +0000

    install pxelinux for ironic_pxe
    pxelinux.0 and chain.c32 files are provided by two
    separate packages on xenial compared to 1 on trusty.
    This change adds the required package to be installed
    and new paths the files reside on xenial.
    
    Change-Id: I48a3622f7257f4cbf994258f82ba91947aae3be4
    Closes-bug: #1629337

--- docker/ironic/ironic-pxe/extend_start.sh
+++ docker/ironic/ironic-pxe/extend_start.sh
@@ -4,7 +4,9 @@
 # of the KOLLA_BOOTSTRAP variable being set, including empty.
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     chown -R ironic: /tftpboot
-    for pxe_file in /var/lib/tftpboot/pxelinux.0 /var/lib/tftpboot/chain.c32 /usr/lib/syslinux/pxelinux.0 /usr/lib/syslinux/chain.c32; do
+    for pxe_file in /var/lib/tftpboot/pxelinux.0 /var/lib/tftpboot/chain.c32 /usr/lib/syslinux/pxelinux.0 \
+                    /usr/lib/syslinux/chain.c32 /usr/lib/PXELINUX/pxelinux.0 \
+                    /usr/lib/syslinux/modules/bios/chain.c32; do
         if [[ -e "$pxe_file" ]]; then
             cp "$pxe_file" /tftpboot
         fi
