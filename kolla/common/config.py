e714a745c65d5729705c71b808e29a2b32c09ae5
Steven Dake <stdake@cisco.com>
2016-10-14 13:23:39 -0700

    Revert "Move RDO to Newton branches"
    Revert note: Get RDO back on master instead of the newton branch
    
    This reverts commit aeb86de6fc644a514d060b4ac869cab364266f03.
    
    Change-Id: I765ba60c3bd09fe3978570b7921a084c9b90c7d0

--- kolla/common/config.py
+++ kolla/common/config.py
@@ -145,7 +145,7 @@ _BASE_OPTS = [
     cfg.StrOpt('maintainer',
                default='Kolla Project (https://launchpad.net/kolla)',
                help='The MAINTAINER field'),
-    cfg.ListOpt('rpm_setup_config',
+    cfg.ListOpt('rpm_setup_config', default=[DELOREAN, DELOREAN_DEPS],
                 help=('Comma separated list of .rpm or .repo file(s) '
                       'or URL(s) to install before building containers')),
     cfg.StrOpt('apt_sources_list', help=('Path to custom sources.list')),
