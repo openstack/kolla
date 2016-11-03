5ddba7892df71d23fefbf7116e8cfec11a2f41da
Paul Bourke <paul.bourke@oracle.com>
2016-11-03 12:31:07 +0000

    Fix plugin parsing for base images
    If we try to add a plugin to a base image, e.g.
    
    [neutron-base-plugin-neutron-fwaas]
    type = git
    location = https://github.com/openstack/neutron-fwaas
    reference = master
    
    The build will fail as it parses the above as both
    "neutron-base-plugin-neutron-fwaas" and "base-plugin-neutron-fwaas".
    
    Update the regex to ensure the image name is anchored to the beginning of the
    conf section.
    
    Change-Id: I4099ebd0d25cd28a9bb3bdc4fcfbf311ce22a3fd
    Closes-Bug: #1638900

--- kolla/image/build.py
+++ kolla/image/build.py
@@ -836,7 +836,7 @@ class KollaWorker(object):
                                             image.name)
                 image.source = process_source_installation(image, image.name)
                 for plugin in [match.group(0) for match in
-                               (re.search('{}-plugin-.+'.format(image.name),
+                               (re.search('^{}-plugin-.+'.format(image.name),
                                           section) for section in
                                 self.conf.list_all_sections()) if match]:
                     try:
