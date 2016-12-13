5276a930b66a831816e44da1f6ea37e0bcc80825
Kuo-tung Kao <jelly.k@inwintack.com>
2016-12-13 14:33:37 +0800

    fix typo in extend_start in collectd docker
    Some typo in extend_start leads to failing to start collectd container.
    fix typo in extend_start in collectd docker.
    
    Change-Id: I41f2524269aa34784e05f08eb6099b5b7016456a
    Closes-bug: #1649481

--- docker/collectd/extend_start.sh
+++ docker/collectd/extend_start.sh
@@ -2,5 +2,5 @@
 
 # Only update permissions if permissions need to be updated
 if [[ $(stat -c %U:%G /var/lib/collectd) != "collectd:collectd" ]]; then
-    sudo chown collectd: /var/lib/collectd
+    sudo chown collectd: /var/lib/collectd/
 fi
