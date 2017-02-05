5ceecfd0a2aaca9bc13f64d3bfabd6034b6fa2b0
Jeffrey Zhang <zhang.lei.fly@gmail.com>
2017-02-05 10:43:27 +0800

    Fix panko log folder owner
    Change-Id: I787d5754925f942d3d31cb4aed8ece4c084b7d6c

--- docker/panko/panko-base/extend_start.sh
+++ docker/panko/panko-base/extend_start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if [[ ! -d "/var/log/kolla/panko" ]]; then
-    mkdir -p /var/log/kolla/panko
+PANKO_LOG_DIR=/var/log/kolla/panko
+
+if [[ ! -d "${PANKO_LOG_DIR}" ]]; then
+    mkdir -p ${PANKO_LOG_DIR}
 fi
-if [[ $(stat -c %a /var/log/kolla/panko) != "755" ]]; then
-    chmod 755 /var/log/kolla/panko
+if [[ $(stat -c %U:%G ${PANKO_LOG_DIR}) != "panko:kolla" ]]; then
+    chown panko:kolla ${PANKO_LOG_DIR}
+fi
+if [[ $(stat -c %a ${PANKO_LOG_DIR}) != "755" ]]; then
+    chmod 755 ${PANKO_LOG_DIR}
 fi
 
 . /usr/local/bin/kolla_panko_extend_start
