d14aadd65dc4d3658777ea5c5878197013a82e92
Eduardo Gonzalez <dabarren@gmail.com>
2017-04-19 10:46:58 +0100

    Fix cinder-api ubuntu binary
    This changes moves cinder-api to make use of apache to launch
    its processes.
    Ubuntu binary removed support in Ocata for cinder-api system
    services [1].
    At this moment cinder in Ocata branch is not working at all
    due this issue.
    This changes aims to fix cinder in master only for ubuntu binary
    with the purpose of make a clean backport to stable/ocata without
    do any other change on the backport process.
    
    A follow up change will migrate other OS/install_type to make use of
    Apache but only changing master branch.
    
    [1] http://lists.openstack.org/pipermail/openstack-dev/2017-January/111069.html
    
    Change-Id: I86ae3ec899e9b7b1ad9f727b9b9bb3549738f944
    Closes-Bug: #1676751

--- docker/cinder/cinder-api/extend_start.sh
+++ docker/cinder/cinder-api/extend_start.sh
@@ -7,3 +7,15 @@ if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     cinder-manage db sync
     exit 0
 fi
+
+if [[ "${KOLLA_INSTALL_TYPE}" == "binary" ]]; then
+    if [[ "${KOLLA_BASE_DISTRO}" =~ debian|ubuntu ]]; then
+        # Loading Apache2 ENV variables
+        . /etc/apache2/envvars
+        rm -rf /var/run/apache2/*
+        APACHE_DIR="apache2"
+    else
+        rm -rf /var/run/httpd/* /run/httpd/* /tmp/httpd*
+        APACHE_DIR="httpd"
+    fi
+fi
