a2c0087b3842c00e27440e69abc3c57125a50cc0
Martin André <m.andre@redhat.com>
2017-05-02 15:14:36 +0200

    Add Apache packages to zaqar container
    Some projects that use Kolla docker containers (TripleO) run zaqar
    under httpd. This patch adds the httpd package so that it exists in the
    container and can optionally by used via Docker entrypoints.
    
    As many of these projects do not require the use of Apache (it is
    opt-in) at this point not all deployment frameworks support the use of
    it so the existing configuration defaults have been left as-is for now.
    
    Change-Id: I35cfd1c2320eb972890b44668c8f9f0a047a65dc
    Partially-Implements: blueprint apache-packages-for-apis

--- docker/zaqar/extend_start.sh
+++ docker/zaqar/extend_start.sh
@@ -6,3 +6,16 @@ fi
 if [[ $(stat -c %a /var/log/kolla/zaqar) != "755" ]]; then
     chmod 755 /var/log/kolla/zaqar
 fi
+
+# Assume the service runs on top of Apache when user is root
+if [[ $USER == 'root' ]]; then
+    # NOTE(pbourke): httpd will not clean up after itself in some cases which
+    # results in the container not being able to restart. (bug #1489676, 1557036)
+    if [[ "${KOLLA_BASE_DISTRO}" =~ debian|ubuntu ]]; then
+        # Loading Apache2 ENV variables
+        . /etc/apache2/envvars
+        rm -rf /var/run/apache2/*
+    else
+        rm -rf /var/run/httpd/* /run/httpd/* /tmp/httpd*
+    fi
+fi
