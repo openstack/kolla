fb003d66ceb46f35675f7e209b18272717841e4c
Eduardo Gonzalez <dabarren@gmail.com>
2017-05-02 13:41:46 +0200

    Add sfc plugin into neutron-base image
    Neutron-server and openvswitch-agent images consume sfc plugin
    code to run sfc services.
    
    This change adds db sync for sfc plugin.
    Also move sfc code into neutron-base so other services
    can make use of the plugin.
    
    Change-Id: I60ba1333231a4ae38a041d41e551f7d74fe15e3b
    Closes-Bug: #1664493

--- kolla/common/config.py
+++ kolla/common/config.py
@@ -488,6 +488,10 @@ SOURCES = {
         'type': 'url',
         'location': ('$tarballs_base/networking-generic-switch/'
                      'networking-generic-switch-master.tar.gz')},
+    'neutron-base-plugin-networking-sfc': {
+        'type': 'url',
+        'location': ('$tarballs_base/networking-sfc/'
+                     'networking-sfc-master.tar.gz')},
     'neutron-base-plugin-vmware-nsx': {
         'type': 'url',
         'location': ('$tarballs_base/vmware-nsx/'
@@ -532,10 +536,6 @@ SOURCES = {
         'type': 'url',
         'location': ('$tarballs_base/neutron-vpnaas/'
                      'neutron-vpnaas-master.tar.gz')},
-    'neutron-sfc-agent': {
-        'type': 'url',
-        'location': ('$tarballs_base/networking-sfc/'
-                     'networking-sfc-master.tar.gz')},
     'neutron-vpnaas-agent': {
         'type': 'url',
         'location': ('$tarballs_base/neutron-vpnaas/'
