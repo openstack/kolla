aee60a2439e40551b76e76d9bd0d127cbbe80945
Steven Dake <stdake@cisco.com>
2017-05-26 21:33:17 -0700

    Remove sudo operations that are no longer necessary
    set_configs.py has logic to handle chown of directories.  Simplify
    the codebase by removing these unnessary chowns.  Further the chowns
    cause some forms of NFS backed storage to not work properly.
    
    Change-Id: I8df95d06b1010778deb3e2a3065aaab26ed2eb6a
    Closes-Bug: #1693973

--- docker/elasticsearch/extend_start.sh
+++ docker/elasticsearch/extend_start.sh
@@ -6,8 +6,3 @@ fi
 if [[ $(stat -c %a /var/log/kolla/elasticsearch) != "755" ]]; then
     chmod 755 /var/log/kolla/elasticsearch
 fi
-
-# Only update permissions if permissions need to be updated
-if [[ $(stat -c %U:%G /var/lib/elasticsearch/data) != "elasticsearch:elasticsearch" ]]; then
-    sudo chown elasticsearch: /var/lib/elasticsearch/data
-fi
