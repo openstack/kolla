11708962aa3ee3a5e6350ffe32ee9e7792393377
Eduardo Gonzalez <dabarren@gmail.com>
2017-06-13 16:28:37 +0200

    Remove bootstrap in etcd
    Doing a chown during bootstrap is useles.
    Is better do it during cotainer startup to ensure
    always have proper permissions.
    
    Change-Id: I9c6c876773288c2f951966498db0ff8af090ac20
    Closes-Bug: #1697699

--- docker/etcd/extend_start.sh
+++ docker/etcd/extend_start.sh
@@ -8,9 +8,6 @@ if [[ $(stat -c %a /var/log/kolla/etcd) != "755" ]]; then
     chmod 755 /var/log/kolla/etcd
 fi
 
-# Bootstrap and exit if KOLLA_BOOTSTRAP variable is set. This catches all cases
-# of the KOLLA_BOOTSTRAP variable being set, including empty.
-if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
+if [[ $(stat -c %U /var/lib/etcd/) != "etcd" ]]; then
     sudo chown etcd: /var/lib/etcd/
-    exit 0
 fi
