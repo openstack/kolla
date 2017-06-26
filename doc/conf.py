b0ff9aaaa5d6c6c6200b91b3543a76dbf7d972de
ZhongShengping <chdzsp@163.com>
2017-06-26 11:48:41 +0800

    Switch from oslosphinx to openstackdocstheme
    As part of the docs migration work[0] for Pike we need to switch to use the
    openstackdocstheme.
    
    [0]https://review.openstack.org/#/c/472275/
    
    Change-Id: Ib82ae202e347c17d255b3c91149474151a59eac3

--- doc/conf.py
+++ doc/conf.py
@@ -21,8 +21,8 @@ sys.path.insert(0, os.path.abspath('../..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+    'openstackdocstheme',
     'sphinx.ext.autodoc',
-    'oslosphinx'
 ]
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
@@ -64,12 +64,15 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-# html_theme_path = ["."]
-# html_theme = '_theme'
+# html_theme_path = []
+html_theme = 'openstackdocs'
 # html_static_path = ['static']
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = '%sdoc' % project
+
+# Must set this variable to include year, month, day, hours, and minutes.
+html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass
@@ -80,3 +83,8 @@ latex_documents = [
      u'%s Documentation' % project,
      u'OpenStack Foundation', 'manual'),
 ]
+
+# openstackdocstheme options
+repository_name = 'openstack/kolla'
+bug_project = 'kolla'
+bug_tag = ''
