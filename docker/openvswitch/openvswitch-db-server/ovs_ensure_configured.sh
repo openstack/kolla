4489bf24d84dc8a4a2489e65117a55857c0b8ade
Pavel Glushchak <pglushchak@virtuozzo.com>
2017-06-27 13:55:51 +0300

    Ensure interface exists before adding OVS port
    Otherwise fail.
    
    Change-Id: Ia84d146e568071a84a7d0e06fcd88e64427904b8
    Closes-Bug: #1700745
    Signed-off-by: Pavel Glushchak <pglushchak@virtuozzo.com>

--- docker/openvswitch/openvswitch-db-server/ovs_ensure_configured.sh
+++ docker/openvswitch/openvswitch-db-server/ovs_ensure_configured.sh
@@ -3,8 +3,14 @@
 bridge=$1
 port=$2
 
-ovs-vsctl br-exists $bridge; rc=$?
-if [[ $rc == 2 ]]; then
+ip link show $port
+if [[ $? -ne 0 ]]; then
+    # fail when device doesn't exist
+    exit 1
+fi
+
+ovs-vsctl br-exists $bridge
+if [[ $? -eq 2 ]]; then
     changed=changed
     ovs-vsctl --no-wait add-br $bridge
 fi
