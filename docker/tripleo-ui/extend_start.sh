9b950f1a9b6ce29d064f4f21b10d5bf8527a7f2b
Honza Pokorny <honza@redhat.com>
2017-10-02 09:50:21 -0300

    Add tripleo-ui image
    Change-Id: Iaf3ecdce44d2bd88fb0132966ba9c786873dbd6a
    Implements: blueprint tripleo-ui-undercloud-container

---
+++ docker/tripleo-ui/extend_start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Assume the service runs on top of Apache when user is root
+if [[ "$(whoami)" == 'root' ]]; then
+    # NOTE(pbourke): httpd will not clean up after itself in some cases which
+    # results in the container not being able to restart. (bug #1489676, 1557036)
+    if [[ "${KOLLA_BASE_DISTRO}" =~ debian|ubuntu ]]; then
+        # Loading Apache2 ENV variables
+        . /etc/apache2/envvars
+        rm -rf /var/run/apache2/*
+    else
+        rm -rf /var/run/httpd/* /run/httpd/* /tmp/httpd*
+    fi
+fi
