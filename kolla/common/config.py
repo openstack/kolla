5a2e5d7f2a2e7f4039391923ca20f785490364d7
Saravanan KR <skramaja@redhat.com>
2017-12-07 15:26:03 +0530

    Add the new user and group for OpenvSwitch 2.8 version
    OvS2.8, by default in fedora/centos/rhel, run as openvswitch:openvswitch
    and when DPDK is enabled, it runs as openvswitch:hugetlbfs. Adding the
    new user and group to the config.
    
    Change-Id: I674cbd45e17906448dd54acfdf7a7059880b7278

--- kolla/common/config.py
+++ kolla/common/config.py
@@ -974,6 +974,14 @@ USERS = {
     'almanach-user': {
         'uid': 42475,
         'gid': 42475,
+    },
+    'openvswitch-user': {
+        'uid': 42476,  # unused user
+        'gid': 42476,
+    },
+    'hugetlbfs-user': {
+        'uid': 42477,  # unused user, but we need the group for vhost socket
+        'gid': 42477,
     }
 }
 
