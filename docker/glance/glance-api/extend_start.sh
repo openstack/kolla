0ac2bc0f71a95c7817f940237b3dec1ae6cbb5bc
Duong Ha-Quang <duonghq@vn.fujitsu.com>
2018-01-09 16:40:01 +0700

    Apply Glance database migration
    Apply Glance database upgrade procedure:
    
    https://docs.openstack.org/glance/latest/admin/zero-downtime-db-upgrade.html
    
    Change-Id: Ife6974db538df8c1461714203974d47a5c577985
    Implements: blueprint apply-service-upgrade-procedure

--- docker/glance/glance-api/extend_start.sh
+++ docker/glance/glance-api/extend_start.sh
@@ -7,3 +7,21 @@ if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     glance-manage db_load_metadefs
     exit 0
 fi
+
+# Migrate database and exit if KOLLA_UPGRADE variable is set. This catches all cases
+# of the KOLLA_UPGRADE variable being set, including empty.
+if [[ "${!KOLLA_UPGRADE[@]}" ]]; then
+    if [[ "${!GLANCE_DB_EXPAND[@]}" ]]; then
+        echo "Expanding database"
+        glance-manage db expand
+    fi
+    if [[ "${!GLANCE_DB_MIGRATE[@]}" ]]; then
+        echo "Migrating database"
+        glance-manage db migrate
+    fi
+    if [[ "${!GLANCE_DB_CONTRACT[@]}" ]]; then
+        echo "Contracting database"
+        glance-manage db contract
+    fi
+    exit 0
+fi
