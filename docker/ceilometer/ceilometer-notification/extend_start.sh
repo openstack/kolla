71a34b6da357d5bc3765423927dcd17fa607aabf
chenyaguang <chenyaguang@szzt.com.cn>
2018-04-03 17:10:29 +0800

    Ceilometer notification container start failed
    There is no need with /etc/apache2/envvars for ceilomter_notification
    
    Change-Id: I00f91dca3763a33f77be2a1968047126504feb55

--- docker/ceilometer/ceilometer-notification/extend_start.sh
+++ docker/ceilometer/ceilometer-notification/extend_start.sh
@@ -12,11 +12,3 @@ if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     sudo chown -R ceilometer: /var/lib/ceilometer/
     exit 0
 fi
-
-if [[ "${KOLLA_BASE_DISTRO}" =~ debian|ubuntu ]]; then
-    # Loading Apache2 ENV variables
-    . /etc/apache2/envvars
-    rm -rf /var/run/apache2/*
-else
-    rm -rf /var/run/httpd/* /run/httpd/* /tmp/httpd*
-fi
