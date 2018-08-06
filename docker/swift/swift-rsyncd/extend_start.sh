6583760fc783825843759de1e995050ee38b7875
Ha Manh Dong <donghm@vn.fujitsu.com>
2018-08-06 16:09:06 +0700

    Revert the patch set "Apply Swift rolling upgrade"
    Due to there is no way to execute swift graceful shutdown commands
    from swift-bootstrap container to swift service containers, so
    we need to revert the patch set at [1].
    
    [1] https://review.openstack.org/#/c/560248/
    
    Change-Id: I3363895347c535054584d63e22284c211ca38ad6

--- docker/swift/swift-rsyncd/extend_start.sh
+++ docker/swift/swift-rsyncd/extend_start.sh
@@ -2,12 +2,3 @@
 
 sudo chown -R swift:swift /srv/node
 mkdir -p /var/lib/swift/lock
-
-# Migrate swift database and exit if KOLLA_STOP_SERVICES variable is set.
-if [[ "${!KOLLA_STOP_SERVICES[@]}" ]]; then
-    echo "Stop background Swift jobs"
-    swift-init rest stop
-    echo "Gracefully Shutdown all Swift storage processes"
-    swift-init {account|container|object} shutdown
-    exit
-fi
