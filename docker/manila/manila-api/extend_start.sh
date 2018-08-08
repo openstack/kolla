6ebcb567541b66d26ae5a00d2abf2178b157bdee
Goutham Pacha Ravi <gouthampravi@gmail.com>
2018-08-08 16:21:06 -0700

    Add support to deploy manila-api with Apache/wsgi
    - Add required packages to run the manila-api service
      with Apache/mod_wsgi, the mod_ssl package has been
      added for RHEL/centos/oraclelinux only.
    - Add necessary cleanup to the start script of the container.
    - Add directories and configuration files that can be
      optionally used via Docker entrypoints.
    
    Change-Id: Id88760655b3419e7b6bec012ebfda16fb92e5ea3
    Implements: bp apache-packages-for-manila-api
    (cherry picked from commit 170be6d17a2e4b85e376a88131ca488c619bebaf)

--- docker/manila/manila-api/extend_start.sh
+++ docker/manila/manila-api/extend_start.sh
@@ -7,3 +7,16 @@ if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
     manila-manage db sync
     exit 0
 fi
+
+# Assume the service runs on top of Apache when user is root
+if [[ "$(whoami)" == 'root' ]]; then
+    # NOTE(pbourke): httpd will not clean up after itself in some cases which
+    # results in the container not being able to restart. (bug #1489676, 1557036)
+    if [[ "${KOLLA_BASE_DISTRO}" =~ debian|ubuntu ]]; then
+        # Loading Apache2 ENV variables
+        . /etc/apache2/envvars
+        rm -rf /var/run/apache2/*
+    else
+        rm -rf /var/run/httpd/* /run/httpd/* /tmp/httpd*
+    fi
+fi
