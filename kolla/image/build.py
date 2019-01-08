43b74ccc157c0b50138c3785ed91dab504895571
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2019-01-08 17:09:52 +0000

    debian/ubuntu: make use of Python3 based packages
    Both Ubuntu Stein UCA and Debian 'buster' migrated their OpenStack
    packages to Python 3.
    
    Note that Debian 'buster' is not released yet and contains Rocky
    packages. Stein ones will be available later.
    
    Co-Authored-By: Lee Yarwood <lyarwood@redhat.com>
    Co-Authored-By: Eduardo Gonzalez <dabarren@gmail.com>
    
    Change-Id: I160f79cc57f54ec3eac857c5babd1a6e2656d228

--- kolla/image/build.py
+++ kolla/image/build.py
@@ -121,7 +121,6 @@ SKIPPED_IMAGES = {
         "nova-mksproxy",
         "novajoin-base",
         "octavia-base",
-        "placement-base",
         # There is no qdrouterd package for ubuntu bionic
         "qdrouterd",
         "searchlight-base",
