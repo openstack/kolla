532af2efc8893cfd23f8d2beadd32a4e3463ae85
Doug Szumski <doug@stackhpc.com>
2019-09-06 14:16:39 +0000

    Do not fan out DB sync to all cells
    Support syncing cell conductor DBs on a per cell basis so that a
    cell can be upgraded in isolation.
    
    Change-Id: I357510a05cacbb9f2cdddbbe9fd06acbce9468ac

--- docker/nova/nova-conductor/extend_start.sh
+++ docker/nova/nova-conductor/extend_start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [[ "${!KOLLA_BOOTSTRAP[@]}" ]]; then
-    nova-manage db sync
+    nova-manage db sync --local_cell
     nova-manage db online_data_migrations
     exit 0
 fi
 
 if [[ "${!KOLLA_UPGRADE[@]}" ]]; then
-    nova-manage db sync
+    nova-manage db sync --local_cell
     exit 0
 fi
 
