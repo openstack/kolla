b8c8fa15edcf5336a1121155cdc880a35ccf161f
Numan Siddique <nusiddiq@redhat.com>
2019-10-27 12:35:47 +0530

    Use ovn-ctl script to start OVN ovsdb-servers
    Using the ovn-ctl script to start the OVN ovsdb-server is the better
    way. This script is used to start and stop all OVN systemd services.
    This script takes care of creating the database from the schema,
    updating the database from the schema when the schema version gets
    updated.
    
    The present scripts - start_nb_db_server.sh and start_sb_db_server.sh
    doesn't take care of schema updates.
    
    With this, we don't need extend_start.sh in ovn-nb-db-server/ovn-sb-db-server
    to create the database from the schema.
    
    This patch also deletes kolla_ovn_extend_start from the ovn-base image as it
    is not required.
    
    Closes-bug: 1850025
    Change-Id: I31a372d801dace92950cd85b29b308431f78bbdb

--- docker/ovn/ovn-sb-db-server/start_sb_db_server.sh
+++ docker/ovn/ovn-sb-db-server/start_sb_db_server.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-ovnsb_db_ip=$1
-ovnsb_port=$2
-if [ -e $ovnsb_port  ]; then
-    ovnsb_port=6642
+# All the option passed to this script will be
+# passed to the ovn-ctl script. Please see the options
+# supported by ovn-ctl script -
+# https://github.com/ovn-org/ovn/blob/master/utilities/ovn-ctl
+args=$@
+
+# Use ovn-ctl script to start ovn SB db server as it
+# takes care of creating the db file from the schema
+# file if the db file is not present. It also takes care
+# of updating the db file if the schema file is updated.
+
+# Check for the presence of ovn-ctl script in two locations.
+# If latest OVN is used (post split from openvswitch),
+# then the new location for the ovn-ctl script is
+# is - /usr/share/ovn/scripts/ovn-ctl. Otherwise it is
+# /usr/share/openvswitch/scripts/ovn-ctl.
+
+
+if [[ -f "/usr/share/openvswitch/scripts/ovn-ctl" ]]; then
+    set /usr/share/openvswitch/scripts/ovn-ctl --no-monitor
+elif [[  -f "/usr/share/ovn/scripts/ovn-ctl" ]]; then
+    set /usr/share/ovn/scripts/ovn-ctl --no-monitor
+else
+    exit 1
 fi
 
-/usr/sbin/ovsdb-server /var/lib/openvswitch/ovnsb.db -vconsole:emer -vsyslog:err -vfile:info \
---remote=punix:/run/openvswitch/ovnsb_db.sock --remote=ptcp:$ovnsb_port:$ovnsb_db_ip \
---unixctl=/run/openvswitch/ovnsb_db.ctl --log-file=/var/log/kolla/openvswitch/ovsdb-server-sb.log
+$@ $args run_sb_ovsdb
