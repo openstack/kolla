8788a452a9ab7c1f3a94986c2a55e887d1530bbf
Mark Goddard <mark@stackhpc.com>
2020-01-13 11:02:51 +0000

    CentOS 8: Don't force tag in build jobs
    We should allow the tag set in kolla-build.conf to be used for the image
    tags, since our deploy jobs need to set it to change_XXXXXXX when images
    are built for the job.
    
    Change-Id: I705514ee0249ff1d309e280001d653271e3686af
    Partially-Implements: blueprint centos-rhel-8

--- tests/test_build.py
+++ tests/test_build.py
@@ -83,11 +83,10 @@ class BuildTestCentos8Binary(BuildTest, base.BaseTestCase):
 
     def setUp(self):
         super(BuildTestCentos8Binary, self).setUp()
-        # TODO(mgoddard): Remove --base-tag and --tag arguments when CentOS 8
-        # is the default.
+        # TODO(mgoddard): Remove --base-tag argument when CentOS 8 is the
+        # default.
         self.build_args.extend(["--base", "centos",
                                 "--base-tag", "8",
-                                "--tag", "master-centos8",
                                 "--type", "binary"])
 
 
@@ -95,11 +94,10 @@ class BuildTestCentos8Source(BuildTest, base.BaseTestCase):
 
     def setUp(self):
         super(BuildTestCentos8Source, self).setUp()
-        # TODO(mgoddard): Remove --base-tag and --tag arguments when CentOS 8
-        # is the default.
+        # TODO(mgoddard): Remove --base-tag argument when CentOS 8 is the
+        # default.
         self.build_args.extend(["--base", "centos",
                                 "--base-tag", "8",
-                                "--tag", "master-centos8",
                                 "--type", "source"])
 
 
