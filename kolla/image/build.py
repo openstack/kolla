052a9433659347a35d4f81b6a07bb83d97a17ad2
Mark Goddard <mark@stackhpc.com>
2020-03-04 17:49:10 +0000

    CentOS 8: Build bifrost image
    Also enables CentOS 8 ansible bifrost job.
    
    Depends-On: https://review.opendev.org/713508
    
    Change-Id: Iec3d51abe50b05f90f764a4058be620cad1f9388
    Partially-Implements: blueprint centos-rhel-8
    (cherry picked from commit a7ba77fa898092bdc6d423391e9478260d476f4c)

--- kolla/image/build.py
+++ kolla/image/build.py
@@ -166,7 +166,6 @@ UNBUILDABLE_IMAGES = {
     },
 
     'centos8+source': {
-        "bifrost-base",          # Bifrost does not support CentOS 8
         "cyborg-agent",          # opae-sdk does not support CentOS 8
         "freezer-base",          # Missing package trickle
     },
