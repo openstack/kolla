8294bac01bf3641452a6a39bbc4f58388838be84
Mark Goddard <mark@stackhpc.com>
2020-03-16 15:00:35 +0000

    CentOS 8: Switch default base image, tag as master
    Changes the default base tag and distro for CentOS images from 7 to 8.
    
    Also changes the tag used by the publisher jobs for CentOS 8 images from
    'master-centos8' to 'master'.
    
    Change-Id: I5498141ade23219d63f34debc2b8e1330dec2e68
    Partially-Implements: blueprint centos-rhel-8

--- kolla/common/config.py
+++ kolla/common/config.py
@@ -23,14 +23,14 @@ from kolla.version import version_info as version
 BASE_OS_DISTRO = ['centos', 'rhel', 'ubuntu', 'debian']
 BASE_ARCH = ['x86_64', 'ppc64le', 'aarch64']
 DEFAULT_BASE_TAGS = {
-    'centos': '7',
-    'rhel': '7',
+    'centos': '8',
+    'rhel': '8',
     'debian': '10',
     'ubuntu': '18.04',
 }
 DISTRO_RELEASE = {
-    'centos': '7',
-    'rhel': '7',
+    'centos': '8',
+    'rhel': '8',
     'debian': '10',
     'ubuntu': '18.04',
 }
