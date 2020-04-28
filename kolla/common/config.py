627beae3b8ddd1de3ddc34637f3e29efcdd53681
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2020-04-28 14:59:45 +0200

    drop tripleo-common-tempest-plugin
    This is not needed plugin without any branches or releases. And wants Python 3
    while we have Py2 for CentOS 7.
    
    Change-Id: I9cab91b82941ea125b16d19e68722ce41a1f11f2
    14:41 < hrw> is  tripleo-common-tempest-plugin your stuff?
    14:43 <@EmilienM> yes and it does nothing. I thought we removed that?
    14:43 <@EmilienM> apparently it's still here https://github.com/openstack/tripleo-common-tempest-plugin
    14:43 <@EmilienM> we should burn it, weshay|ruck ^
    14:44 < hrw> EmilienM: also fine to drop from train?
    14:44 <@EmilienM> burn it everywhere you can
    14:44 < hrw> THANKS
    14:44 <@EmilienM> it has never been useful
    14:44 <@EmilienM> and people who started it left
    14:48 <@weshay|ruck> EmilienM, yup

--- kolla/common/config.py
+++ kolla/common/config.py
@@ -749,10 +749,6 @@ SOURCES = {
         'type': 'url',
         'location': ('$tarballs_base/telemetry-tempest-plugin/'
                      'telemetry_tempest_plugin-0.3.0.tar.gz')},
-    'tempest-plugin-tripleo-common': {
-        'type': 'url',
-        'location': ('$tarballs_base/tripleo-common-tempest-plugin/'
-                     'tripleo-common-tempest-plugin-master.tar.gz')},
     'tempest-plugin-trove': {
         'type': 'url',
         'location': ('$tarballs_base/trove-tempest-plugin/'
