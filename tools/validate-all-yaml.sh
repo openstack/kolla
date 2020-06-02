712b258f2d1870b3f07fb67b043769999edb3ef5
mnasiadka <mnasiadka@gmail.com>
2020-06-02 08:16:52 +0000

    Fix pre-commit-hook
    Currently pre-commit-hook is failing due to yaml validations using
    unversioned python and using non-existent json validations.
    
    Change-Id: I67dc11efe667e2cc67ea1a731edc5c4a03ebd1e7
    Closes-Bug: #1778146

--- tools/validate-all-yaml.sh
+++ tools/validate-all-yaml.sh
@@ -5,4 +5,4 @@ REAL_PATH=$(realpath $0)
 cd "$(dirname "$REAL_PATH")/.."
 
 find . -name '*.yaml' -o -name '*.yml' -print0 |
-    xargs -0 python tools/validate-yaml.py || exit 1
+    xargs -0 python3 tools/validate-yaml.py || exit 1
