032804e5a0ddf89f7726880d1b57dd492d7d5c07
Radosław Piliszek <radoslaw.piliszek@gmail.com>
2020-10-04 11:26:41 +0200

    Be strict about errors in start
    Make start.sh run with pipefail and nounset to avoid common errors
    in the start scripts and detect them early.
    
    Httpd code had to be patched to allow it to pass on Debuntu.
    
    Also fix the two missed applications of httpd to make sure all
    its path are covered.
    
    And also fix Horizon's ENABLE_ZAQAR - K-A does not use Zaqar.
    Yet another - Horizon's settings_bundle. :-)
    
    Finally, fix Neutron for Debuntu (KOLLA_LEGACY_IPTABLES).
    
    Change-Id: I39b8d78f6758df1f92b8b0d2c06ea99b038b843b
    Depends-On: https://review.opendev.org/711923

--- docker/neutron/neutron-base/extend_start.sh
+++ docker/neutron/neutron-base/extend_start.sh
@@ -9,6 +9,11 @@ fi
 
 # set legacy iptables to allow kernels not supporting iptables-nft
 if /usr/bin/update-alternatives --query iptables; then
+    # NOTE(yoctozepto): Kolla-Ansible does not always set KOLLA_LEGACY_IPTABLES;
+    # the workaround below ensures it gets set to `false` in such cases to fix
+    # this code under `set -o nounset`.
+    KOLLA_LEGACY_IPTABLES=${KOLLA_LEGACY_IPTABLES-false}
+
     if [[ $KOLLA_LEGACY_IPTABLES == "true" ]]; then
         sudo /usr/bin/update-alternatives --set iptables /usr/sbin/iptables-legacy
         sudo /usr/bin/update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
