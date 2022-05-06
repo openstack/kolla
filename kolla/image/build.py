3d837170ea7196a08692a5f9f715e8b41da189a8
Marcin Juszkiewicz <marcin.juszkiewicz+kolla@linaro.org>
2022-05-06 07:45:56 +0200

    cloudkitty: disable building for ubuntu/binary
    There are no packages in UCA.
    
    http://lists.openstack.org/pipermail/openstack-discuss/2022-May/028419.html
    
    Change-Id: Iaf5ce2b7276b275b21d892d0044a126ab901507f

--- kolla/image/build.py
+++ kolla/image/build.py
@@ -142,6 +142,7 @@ UNBUILDABLE_IMAGES = {
     },
 
     'ubuntu+binary': {
+        "cloudkitty-base",  # no binary packages in UCA
         "senlin-conductor",  # no binary package
         "senlin-health-manager",  # no binary package
         "tacker-base",
