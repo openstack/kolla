d1d26c867493800b83827e991483c447b89ff87a
Michal Nasiadka <mnasiadka@gmail.com>
2022-12-01 09:41:57 +0100

    telegraf: mark buildable
    Change-Id: Iff646c87a5c8d8d1831fea00ae18457f93cca2e5

--- kolla/image/unbuildable.py
+++ kolla/image/unbuildable.py
@@ -27,8 +27,6 @@ UNBUILDABLE_IMAGES = {
     # Issues for SHA1 keys:
     # https://github.com/elastic/elasticsearch/issues/85876
     # https://github.com/grafana/grafana/issues/41036
-    # Issue with telegraf:
-    # https://github.com/influxdata/telegraf/issues/12303
     'centos': {
         "elasticsearch",         # SHA1 gpg key
         "hacluster-pcs",         # Missing crmsh package
@@ -37,7 +35,6 @@ UNBUILDABLE_IMAGES = {
         "nova-spicehtml5proxy",  # Missing spicehtml5 package
         "ovsdpdk",               # Not supported on CentOS
         "tgtd",                  # Not supported on CentOS
-        "telegraf",              # Package is not signed
     },
 
     'debian': {
@@ -51,7 +48,6 @@ UNBUILDABLE_IMAGES = {
         "nova-spicehtml5proxy",  # Missing spicehtml5 package
         "ovsdpdk",               # Not supported on CentOS
         "tgtd",                  # Not supported on CentOS
-        "telegraf",              # Package is not signed
     },
 
     'ubuntu': {
