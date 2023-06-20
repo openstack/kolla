1e0cef81af47d915ae1230e92661bcf24b010515
Maksim Malchuk <maksim.malchuk@gmail.com>
2023-06-20 13:06:48 +0300

    Disable build of Gnocchi containers
    Revert this commit when [1] is solved.
    
    1. https://github.com/gnocchixyz/gnocchi/issues/1304
    
    Change-Id: Iafeb9093340621d65a9c9cd126d65679d981dfb1
    Signed-off-by: Maksim Malchuk <maksim.malchuk@gmail.com>
    (cherry picked from commit 44e04967afcde2424af010eddd4ddc8b7e4e1eb7)

--- kolla/image/build.py
+++ kolla/image/build.py
@@ -90,12 +90,17 @@ UNBUILDABLE_IMAGES = {
         "blazar-base",
         "cyborg-base",
         "freezer-base",
+        "gnocchi-base",  # https://github.com/gnocchixyz/gnocchi/issues/1304
         "kuryr-base",
         "monasca-base",
         "monasca-thresh",
         "solum-base",
         "vmtp",
         "zun-base",
+    },
+
+    'source': {
+        "gnocchi-base",  # https://github.com/gnocchixyz/gnocchi/issues/1304
     },
 
     'centos': {
