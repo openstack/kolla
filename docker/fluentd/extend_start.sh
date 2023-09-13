c0aee4adb69e25f0e75fc9f907cf962ef024f358
Sven Kieske <kieske@osism.tech>
2023-09-13 19:38:59 +0200

    bump td-agent lts from v4 to v5
    notice that the td-agent command got renamed to fluentd.
    see also the upgrade notes at[0].
    
    for more details, refer to the changelog[1].
    
    [0]: https://web.archive.org/web/20231006084137/https://www.fluentd.org/blog/upgrade-td-agent-v4-to-v5
    [1]: https://github.com/fluent/fluent-package-builder/blob/b4b7017c09d78f2c404db62612733b40f69668a6/CHANGELOG.md#release-v500---20230729
    
    Depends-On: https://review.opendev.org/c/openstack/kolla-ansible/+/901509
    Change-Id: I5e95363c6c180e330945fe3d98e97c7049bbd6f7
    Signed-off-by: Sven Kieske <kieske@osism.tech>

--- docker/fluentd/extend_start.sh
+++ docker/fluentd/extend_start.sh
@@ -6,23 +6,15 @@
 # Also set the setgid permission on the /var/log/kolla directory so that new
 # files and sub-directories in that directory inherit its group id ("kolla").
 
-if [[ "${KOLLA_BASE_ARCH}" == "x86_64" && "${KOLLA_BASE_DISTRO}" != "debian" ]]; then
-    USERGROUP="td-agent:kolla"
-    FLUENTD="td-agent"
-else
-    USERGROUP="fluentd:kolla"
-    FLUENTD="fluentd"
-fi
-
 if [ ! -d /var/log/kolla ]; then
     mkdir -p /var/log/kolla
 fi
-if [[ $(stat -c %U:%G /var/log/kolla) != "${USERGROUP}" ]]; then
-    sudo chown ${USERGROUP} /var/log/kolla
+if [[ $(stat -c %U:%G /var/log/kolla) != "fluentd:kolla" ]]; then
+    sudo chown fluentd:kolla /var/log/kolla
 fi
 if [[ $(stat -c %a /var/log/kolla) != "2775" ]]; then
     sudo chmod 2775 /var/log/kolla
 fi
-if [[ (-d /var/lib/${FLUENTD}) && ($(stat -c %U:%G /var/lib/${FLUENTD}) != "${USERGROUP}") ]]; then
-    sudo chown ${USERGROUP} /var/lib/${FLUENTD}
+if [[ (-d /var/lib/fluentd) && ($(stat -c %U:%G /var/lib/fluentd) != "fluentd:kolla") ]]; then
+    sudo chown fluentd:kolla /var/lib/fluentd
 fi
