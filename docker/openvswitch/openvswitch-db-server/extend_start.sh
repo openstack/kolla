e8a6a457211b58ed966ed2faab0266bb8e3a7595
Michal Nasiadka <mnasiadka@gmail.com>
2024-01-09 14:12:00 +0100

    openvswitch: Move db conversion from kolla-ansible
    Partial-Bug: #2048130
    Change-Id: I2f6c9045aa990b36716bb18d9f2eb699a35ee51a

--- docker/openvswitch/openvswitch-db-server/extend_start.sh
+++ docker/openvswitch/openvswitch-db-server/extend_start.sh
@@ -4,3 +4,7 @@ mkdir -p "/run/openvswitch"
 if [[ ! -e "/var/lib/openvswitch/conf.db" ]]; then
     ovsdb-tool create "/var/lib/openvswitch/conf.db"
 fi
+
+if [[ $(ovsdb-tool needs-conversion /var/lib/openvswitch/conf.db) == yes ]]; then
+    ovsdb-tool convert "/var/lib/openvswitch/conf.db"
+fi
