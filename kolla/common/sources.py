7e95ffb09815d1918b3b262e074d325b4fb35d70
Michal Nasiadka <mnasiadka@gmail.com>
2024-04-15 13:14:37 +0200

    gnocchi: Bump to 4.6.2
    This fixes werkzeug breakage [1].
    
    [1]: https://github.com/gnocchixyz/gnocchi/releases/tag/4.6.2
    
    Change-Id: I5d0659c9dffd6b7a3004696440d2879d60f327ea

--- kolla/common/sources.py
+++ kolla/common/sources.py
@@ -80,7 +80,7 @@ SOURCES = {
                      'glance-${openstack_branch}.tar.gz')},
     'gnocchi-base': {
         'type': 'git',
-        'reference': '4.6.1',
+        'reference': '4.6.2',
         'location': ('https://github.com/gnocchixyz/'
                      'gnocchi.git')},
     'heat-base': {
