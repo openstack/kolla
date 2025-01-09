c1ce31adf79e648b5d960868f000186eed5cffe5
Bartosz Bezak <bartosz@stackhpc.com>
2025-01-09 12:49:25 +0100

    fix sources.py based cross-arch builds
    Derive debian_arch from base_arch by default when not set by the user.
    This ensures that cross-architecture builds correctly download
    the appropriate binaries, while preserving any explicit user override.
    
    Closes-Bug: #2103810
    
    Change-Id: I11b7e19ac3a12147138a98fc1e030767f70b44c9
    (cherry picked from commit 915189d10be83136d54f4216b81310b3dd16b82d)

--- kolla/common/config.py
+++ kolla/common/config.py
@@ -386,6 +386,9 @@ def parse(conf, args, usage=None, prog=None,
     openstack_branch_slashed = openstack_branch.replace('-', '/')
     conf.set_default('openstack_branch', openstack_branch)
     conf.set_default('openstack_branch_slashed', openstack_branch_slashed)
+    # NOTE(bbezak) Derive debian_arch from base_arch if not set explicitly
+    derived_arch = 'arm64' if conf.base_arch == 'aarch64' else 'amd64'
+    conf.set_default('debian_arch', derived_arch)
 
     if not conf.base_image:
         conf.base_image = DEFAULT_BASE_TAGS[conf.base]['name']
