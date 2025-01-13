771272100b9f66df4ea15fc9cbc5c4e86682c0d5
Michal Nasiadka <mnasiadka@gmail.com>
2025-01-13 09:54:25 +0100

    openvswitch: Fix logs dir permission
    Closes-Bug: #1795609
    
    Change-Id: Iedfb2df027c0cefc45668a76d45ce62193aba9ef
    (cherry picked from commit a9c94abf530f3ed36b9b3f10c89549f04af26df4)

--- docker/openvswitch/openvswitch-vswitchd/extend_start.sh
+++ docker/openvswitch/openvswitch-vswitchd/extend_start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [[ ! -r "/var/log/kolla/openvswitch/ovs-vswitchd.log" ]]; then
+    touch /var/log/kolla/openvswitch/ovs-vswitchd.log
+    chown openvswitch:openvswitch /var/log/kolla/openvswitch/ovs-vswitchd.log
+fi
+
 modprobe openvswitch
