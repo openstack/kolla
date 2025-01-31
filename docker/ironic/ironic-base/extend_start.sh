ed7c8399c4a2a43e9a4165db416b98f5be8d538c
Michal Arbet <michal.arbet@ultimum.io>
2025-01-31 12:48:45 +0100

    Fix permissions for ironic metrics
    This patch adds creation of metrics folder, set
    permissions  and  SetGID bit.
    
    Closes-Bug: #2097098
    Change-Id: Ic46b895775edf5e5fb2b637be49e2de1eb4adf36

--- docker/ironic/ironic-base/extend_start.sh
+++ docker/ironic/ironic-base/extend_start.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 LOG_PATH=/var/log/kolla/ironic
+METRICS_PATH=/var/lib/ironic-metrics
 
 if [[ ! -d "${LOG_PATH}" ]]; then
     mkdir -p "${LOG_PATH}"
 fi
+if [[ ! -d "${METRICS_PATH}" ]]; then
+    sudo mkdir -p "${METRICS_PATH}"
+fi
 if [[ $(stat -c %a "${LOG_PATH}") != "755" ]]; then
     chmod 755 "${LOG_PATH}"
+fi
+if [[ $(stat -c %U:%G "${METRICS_PATH}") != "ironic:ironic" ]]; then
+    sudo chown ironic:ironic "${METRICS_PATH}"
+fi
+if [[ $(stat -c %a "${METRICS_PATH}") != "2775" ]]; then
+    sudo chmod 2775 "${METRICS_PATH}"
 fi
 
 . /usr/local/bin/kolla_ironic_extend_start
