b0714b2e98e1a46a445c3ca8ae4b9dc85bfdeef3
Michal Nasiadka <mnasiadka@gmail.com>
2025-07-14 07:36:40 +0200

    heat: Ensure log files are owned by heat user
    Change-Id: I4567aaf352f1f590af2fa7d067768799adad64d1
    Signed-off-by: Michal Nasiadka <mnasiadka@gmail.com>

--- docker/heat/heat-base/extend_start.sh
+++ docker/heat/heat-base/extend_start.sh
@@ -3,6 +3,9 @@
 if [[ ! -d "/var/log/kolla/heat" ]]; then
     mkdir -p /var/log/kolla/heat
 fi
+if [[ $(stat -c %U:%G /var/log/kolla/heat) != "heat:kolla" ]]; then
+    chown -R heat:kolla /var/log/kolla/heat
+fi
 if [[ $(stat -c %a /var/log/kolla/heat) != "755" ]]; then
     chmod 755 /var/log/kolla/heat
 fi
